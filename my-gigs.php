b758626ed697726ce13882b7434aa4c37ac693c3
Amol <amol.chawathe@fountaintechies.com>
2014-10-19 23:17:05 +0800

    My Gigs - This is where the fun starts

--- my-gigs.php
+++ my-gigs.php
@@ -22,7 +22,7 @@ include('cfg/more-functions.php');
                          <li><a href="assignments">My Assigments</a></li>
                        </ul>
                </section>
-       <section class="container">
+  <section class="container">
         <ul id="profilemenu">
           <li><a href="mygigs-inprogress"><h5 id="ass"> In progress </h5> </a></li>
           <li><a href="mygigs-bidding"> Bidding  </a></li>
@@ -32,82 +32,7 @@ include('cfg/more-functions.php');
       <section class="container">
       <h2 id="giglog">My Gigs</h2>
 
-      <?php $mygigs=get_user_gigs($uInfo['userId']);
-	if($mygigs['count']>0)
-	{
-		for($i=0;$i<$mygigs['count'];$i++)
-		{
-			$mygig=$mygigs['rows'][$i];
-			$projectbids=get_project_bids($mygig['prjId']);
-			$bidcount=$projectbids['count'];
-			if(!$bidcount)
-			{
-				$bidcount="0";
-			}
-			
-	  ?>
-        <div class="row mygig">
-           <div class="col-md-10">
-             <h2>
-             <a href="<?php echo $serverpath;?>gigDetails/<?php echo urlencode($mygig['prjTitle']);?>/<?php echo $mygig['prjId'];?>"><?php echo $mygig['prjTitle'];?></a></h2>
-             <h3 class="selectbid">Select Bidder</h3>
-             <?php 
-				if($projectbids['count']>0)
-				{
-					for($h=0;$h<$projectbids['count'];$h++)
-					{
-						$biduser=get_user_Info(encrypt_str($projectbids['rows'][$h]['bidfrom']));
-						
-						$biduserprofilepic="uploads/profileimage/".$biduser['profileimage'];
-							if(file_exists($biduserprofilepic))
-							{
-								$biduserprofilepic=$biduserprofilepic;
-							}
-							else
-							{
-								$biduserprofilepic="images/admin.png";
-							}
-							 $biddernametodisplay="";
-							 $biddernametodisplay=$biduser['fname'].' '.$biduser['lname'];
-							 $biddernametodisplay1=str_replace(" ","",$biddernametodisplay);
-							  if(!$biddernametodisplay1)
-							  {
-								  $biddernametodisplay=$biduser['username'];
-							  }
-							?>
-							 <a href="<?php echo $serverpath;?>gigsterInfo/<?php echo urlencode($biddernametodisplay);?>/<?php echo $biduser['userId'];?>" title="<?php echo $biddernametodisplay;?>"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $biduserprofilepic;?>&width=80&height=80&cropratio=1:1"></a>
-							<?php
-					}
-				}
-				else
-				{
-					?>
-					<p class="mandatory">Sorry, No Bids Submited yet.</p>
-					<?php
-				}
-			 ?>
-           </div>
-           <div class="col-md-2 rightsidegig" align="right">
-            <h4 class="firstbid"><?php echo $bidcount;?> Bids</h4>
-            <h5>S &#36; <?php echo $mygig['proposedbudget'];?></h5>
-           </div>
-           <div class="clearline"></div>
-        </div>
-     <?php
-		}
-	}
-	else{
-		?>
-		<div class="row mygig">
-           <div class="col-md-10">
-             <h2 class="mandatory">Sorry, No Gigs posted by you yet.</h2>
-           </div>
-           
-           <div class="clearline"></div>
-        </div>
-		<?php
-	}
-	 ?>   
+     This is a test!!
      </section>
     <?php include('footer.php'); ?>
   </body>
