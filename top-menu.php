9237b771efe7fb1af20c140a737a230a7fd6163e
saurabhundre <saurabh@fountaintechies.com>
2014-10-28 17:33:49 -0700

    inbox added
    inbox added

--- top-menu.php
+++ top-menu.php
@@ -1,10 +1,10 @@
 <header>      
       <div class="container">
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <a href="<?php echo $serverpath;?>"><img src="<?php echo $serverpath;?>images/logo.png"></a>
           </div>
-          <div class="col-md-8">
+          <div class="col-md-9">
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                 <?php if(isset($_SESSION['uId']))
