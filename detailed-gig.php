fc8b9cf1f9f9f397c16bff17c7528a2a4f90813f
saurabhundre <saurabh@fountaintechies.com>
2014-10-29 22:20:57 -0700

    mail image added
    mail image added

--- detailed-gig.php
+++ detailed-gig.php
@@ -250,6 +250,12 @@ else
         <?php if((encrypt_str($gigdetails['userId'])==$_SESSION['uId']) || is_message_thread_initiated($gigdetails['prjId'],$bidderInfo['userId']))
 				 {
 			?>
+      <div>
+                   <img src="images/mail.jpg">
+                 </div>
+                 <div>
+                   <img src="images/symbol.png">
+                 </div> 
         <a href="#msgmodal<?php echo $gigdetails['userId'];?>" data-toggle="modal"><img src="<?=$serverpath;?>images/mail.jpg"></a>
         <div id="msgmodal<?php echo $gigdetails['userId'];?>" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
