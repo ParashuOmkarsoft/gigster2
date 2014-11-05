5c7902a3837db6b85114bdb02f9c23766e2f336a
amaninfodreams <aman.infodreams@gmail.com>
2014-11-05 11:28:23 +0530

    j26

--- user-bided.php
+++ user-bided.php
@@ -77,6 +77,87 @@ if($checkSql['count']>0)
       </div>
       <div class="col-md-8"> 
        <span class="budget"><?php echo $prjDetails['proposedbudget']; ?> <?php echo $currency ; ?></span>
+       </a> <a href="#msgmodal<?php echo $prjDetails['userId'];?>" data-toggle="modal"><img src="<?=$serverpath;?>images/mail.jpg"></a>
+       <div id="msgmodal<?php echo $prjDetails['userId'];?>" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content cform">
+              <div class="container">
+                <div class="col-md-12">
+                  <form class="form-horizontal postgigforminner" action="<?php echo $serverpath;?>sendmessage" target="targetframe" role="form" method="post" >
+                    <input type="hidden" id="fromId" name="fromId" value="<?php echo $uInfo['userId'];?>" />
+                    <input type="hidden" id="toId" name="toId" value="<?php echo $prjDetails['userId'];?>" />
+                    <input type="hidden" id="projectId" name="projectId" value="<?php echo $prjDetails['prjId'];?>" />
+                    <h2 id="login1">Messages</h2>
+                    <h2 class="source"><?php echo $prjDetails['prjTitle'];?></h2>
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label class="col-md-4 control-label tfont">Message</label>
+                        <Br/>
+                        <br/>
+                        <div class="col-md-12">
+                          <textarea name="message" id="message" class="form-control mtextarea" ></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-10 logsign">
+                          <button type="submit" class="btn btn-warning loginbtn">Send Message</button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div class="col-md-12" style="height:250px; overflow:scroll;">
+                  <?php
+			$muInfo=get_user_Info($_SESSION['uId']);
+			$muId=$muInfo['userId'];
+			$other=get_oponent($pId,$muId);
+		 	$projectMessages="select * from btr_messages where projectId=".$prjDetails['prjId']." and (msgfrom=$muId or msgto=$muId) order by msgId DESC";
+			$projectMessages=@db_query($projectMessages);
+			$messages=$projectMessages;
+			for($t=0;$t<$messages['count'];$t++)
+			{
+			$msgfrom=$messages['rows'][$t]['msgfrom'];
+			$fromInfo=get_user_Info(filter_text(encrypt_str($msgfrom)));
+			$fromuserimg=$fromInfo['profileimage'];
+			$buserimage="";
+			if(!$fromuserimg)
+			{
+				$buserimage=filter_text('img/avatar5.png');
+			}
+			else
+			{
+				$buserimage="uploads/profileimage/".$fromuserimg;
+			}
+			if($t%2==0)
+			{
+				$cl="style='background-color:#f8f8f8;padding:5px;border-radius:10px;-moz-box-shadow: 0px 0px 2px #000000;
+-webkit-box-shadow: 0px 0px 2px #000000;
+box-shadow: 0px 0px 2px #000000;'";
+			}
+			else
+			{
+				$cl="style='background-color:white;padding:5px;border-radius:10px;-moz-box-shadow: 0px 0px 2px #000000;
+-webkit-box-shadow: 0px 0px 2px #000000;
+box-shadow: 0px 0px 2px #000000;'";
+			}
+			$updatemessage=@db_query("update btr_messages set isread='1' where msgId=".$messages['rows'][$t]['msgId']);	
+			?>
+                  <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="online"/> <br/>
+                    <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small><br/>
+                      <?php echo get_user_name($msgfrom);?> </a><br/>
+                      <?php echo stripslashes(stripslashes(html_entity_decode($messages['rows'][$t]['msgcontent'])));
+					  ?><br/>
+                    </p>
+                  </div>
+                  <br/>
+                  <?php
+		}
+		  ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row">
