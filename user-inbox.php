794de53e21e1e72e9f261e940059fd5d3483e530
saurabhundre <saurabh@fountaintechies.com>
2014-11-06 12:55:05 +0530

    inbox msg check-box remove
    inbox msg check-box remove

--- user-inbox.php
+++ user-inbox.php
@@ -26,182 +26,152 @@ include('cfg/more-functions.php');
 
 <div class="row" style="max-width: 938px;padding-left: 30px;">
   <div class="row clearfix" style="background: #FFEDAF;padding: 10px;border-bottom: 1px solid #FFD09A">
-    <div class="col-md-1 column" style="width: 20px;">
+    <div class="col-md-1" style="width: 20px;">
      <!-- <input type="checkbox"> -->
          </div>
-    <div class="col-md-1 column"style="width:20px">
+    <div class="col-md-1"style="width:20px">
       <!-- <img src="images/star_1.png"> -->
     </div>
-    <div class="col-md-2 column" style="width: 66px;">
+    <div class="col-md-2" style="width: 66px;">
       <!-- <img src="images/person2.jpg" style="width: 50px;"> -->
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left">
-       SENDER
+       GIG
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
         LAST MESSAGE
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
       UPDATED
       </h5>
     </div>
   </div>
     <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <a href="#"><img src="images/star_1.png"></a>
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      No problem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
     </div>
   </div>
-    <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <a href="#"><img src="images/star_1.png"></a>
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+       <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      No problem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
     </div>
   </div>
-  <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <img src="images/star_1.png">
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+     <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      No problem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
     </div>
   </div>
-  <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <img src="images/star_1.png">
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+     <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      No problem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
     </div>
   </div>
-  <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <img src="images/star_1.png">
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+     <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      No problem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
     </div>
   </div>
-  <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
-    <div class="col-md-1 column" style="width: 20px;">
-     <input type="checkbox">
-         </div>
-    <div class="col-md-1 column"style="width:20px">
-      <img src="images/star_1.png">
-    </div>
-    <div class="col-md-2 column" style="width: 66px;">
+     <div class="row clearfix" style="background: #F9F9F9;padding: 10px;border-bottom: 1px solid #FFD09A;">
+ 
+    <div class="col-md-2" style="width: 125px;">
       <img src="images/person2.jpg" style="width: 50px;">
     </div>
-    <div class="col-md-2 column" style="">
+    <div class="col-md-2" style="">
       <h5 class="text-left text-info">
        mike fisher
       </h5>
     </div>
-    <div class="col-md-5 column">
+    <div class="col-md-5">
       <h5>
-      hello..
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
       </h5>
     </div>
-    <div class="col-md-1 column pull-right" style="width: 175px;">
+    <div class="col-md-1 pull-right" style="width: 175px;">
       <h5>
        Nov 14,2014
       </h5>
