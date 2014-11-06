cb20b546d1ec9048124e69e0307863c91b6212e8
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-11-06 18:35:31 +0530

    user inbox

--- user-inbox.php
+++ user-inbox.php
@@ -24,15 +24,75 @@ include('cfg/more-functions.php');
         <div id="our">Inbox</div>
         <div></div>
 <?php 
-$query="select DISTINCT(projectId) from btr_messages where msgto=".$uInfo['userId']." order by msgId DESC";
+ $query="select * from btr_messages where msgto=".$uInfo['userId']." order by msgId DESC";
+
 $sql=@db_query($query);
 if($sql['count']>0)
 {
+
+
 ?>
-<table cellpadding="0"  cellspacing="0">
-</table>
-<?php 
-}
+<table class="table table-striped" id="mydatatable">
+<thead>
+  <tr>
+    <th>#</th>
+    <th>Gig Title</th>
+    <th>From</th>
+    <th>Message</th>
+    <th>Sent On</th>
+
+  </tr>
+  <?php
+  $sno=1;
+	$mprojects="";
+	$mcount=0;
+  for($i=0;$i<$sql['count'];$i++)
+  {
+	if(in_array($sql['rows'][$i]['projectId'],$mprojects))
+	{
+	}
+	else
+	{
+			
+	    $prjDetails=get_gig_details($sql['rows'][$i]['projectId']);
+		if($prjDetails['prjId'])
+		{	  
+	  	$mprojects[$mcount]=$sql['rows'][$i]['projectId'];
+		$fromdetails=get_user_Info(encrypt_str($sql['rows'][$i]['msgfrom']));
+		$nametodisplay=$fromdetails['fname']." ".$fromdetails['lname'];
+	$nametodisplay1=str_replace(" ","",$nametodisplay);
+	if(!$nametodisplay1)
+	{
+		$nametodisplay=$fromdetails['username'];
+	}
+	$st="";
+	if($sql['rows'][$i]['isread'])
+	{
+		$st="style='font-weight:bold'";
+	}
+	else
+	{
+		$st="";
+	}
+	  ?>
+      
+	  <tr <?php echo $st;?>>
+      	<td><?php echo $sno;?></td>
+		<td><a href="<?php echo $serverpath;?>messagethread/<?php echo $sql['rows'][$i]['msgId'];?>"><?php echo $prjDetails['prjTitle'];?></a> </td>                
+		<td><?php echo $nametodisplay;?></td> 
+        <td><?php echo strip_string($sql['rows'][$i]['msgcontent'],50);?></td>
+        <td><?php echo get_time($sql['rows'][$i]['msgon']);?></td>                                               
+      </tr>
+	  <?php
+	
+	  $sno++;
+		}
+	}
+  }
+  ?>
+  </thead>
+  </table>
+<?php  }
 ?>
      </section>
     <!--<section class="container giglast">
