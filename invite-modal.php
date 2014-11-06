4ea4d6cdf5d6d34aa703560665e0dd76f1ad6179
saurabhundre <saurabh@fountaintechies.com>
2014-11-06 18:11:54 +0530

    invite-post

--- invite-modal.php
+++ invite-modal.php
@@ -2,78 +2,127 @@
 <div id="invitemodal" class="modal fade  bs-example-modal-lg modelw " tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-        <section class="container">
-      <h2 id="logingigster1">Open Gigs</h2>
-      <div class="row firstrow">
-         <div class="col-md-8">
-            <h2 id="giglisth2">Looking for a marriage photographer</h2>
-            <h2 id="map">Yishun, Singapore</h2>
-              <div class="col-md-4"><span id="bid">6 Bids</span></div>
-              <div class="col-md-8"><span class="bid">Posted : 2 hours ago<span></div>
-              <p id="gigpara">Hy there I am looking for a special photo editing app for I-Phone. My friend already
-                  has this but he kept it as secret. I have some pics for you as exampel..... 
-                  Best regards Erika</p>
-          </div>
-         <div class="col-md-4 giginnerimg gigimg">
-              <div class="col-md-9">
-                   <img src="images/star_1.png" style="float: right;">
-                    <img src="images/star_1.png" style="float: right;">
-                     
-                   <h4>Saurabh Undre</h4>
+        <section class="postgigform" id="postgigform">                       
+         <h2 id="login1">Post a Gig </h2>    
+          <h2 class="source">Post a new Gig for free. Invite Gigsters to bid on your gig.</h2>                          
+          <form class="form-horizontal postgigforminner" action="<?php echo $serverpath;?>saveGig" role="form" method="post"  >
+            <div class="form-group">
+              <label for="creategig" class="col-sm-2 control-label labelb">Title</label>
+              <div class="col-sm-10"> 
+                <input type="text" required class="form-control" id="prjTitle" name="prjTitle" placeholder="Add title for you gig" >
               </div>
-              <div class="col-md-3">
-                   <img src="images/person1.jpg" style="margin-left: -20px;">
-              </div>
-
-
-           <!-- Bid Model -->
-            <a data-toggle="modal" href="#bidmodel" ><button type="button" class="btn btn-warning" style="margin-left: 125px">Bid</button></a>  
             </div>
-            <!-- start bid model -->
-               <div id="bidmodel" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
-                          <div class="modal-dialog modal-lg">
-                            <div class="modal-content cform">          
-                              <div class="container">
-                                  <div class="col-md-12">
-
-                                            <h2 id="login1">Bid On Gig </h2>    
-                                            <h2 class="source">Lady Fashion Model Shoot</h2>  
-                                              <div class="col-md-8"><form class="form-horizontal hform" role="form">
-                                      
-                                            <div class="form-group">
-                                              <label for="inputText" class="col-sm-4 control-label newlog">Bid Details</label>
-                                               <div class="col-sm-8">
-                                                 <textarea class="form-control tinpute" placeholder="Bid Details" row="10" column="10"></textarea>
-                                               </div>
-                                            </div>
-                                            <div class="form-group">
-                                              <label class="col-md-4 control-label tfont">Prize</label>
-                                              <div class="col-md-8">
-                                                <input class="form-control tinpute" type="text" value="">
-                                              </div>
-                                            </div>
-                                            
-                                            <div class="form-group">
-                                              <div class="col-sm-offset-3 col-sm-10 logsign">
-                                                <button type="submit" class="btn btn-warning loginbtn">Bid Now</button>
-                                              </div>
-                                            </div>
-
-                                    </div>
-                                          
-                                 </form>
-                               </div>
-                             </div>
-                           </div>          
-                        
-                </div>
+            <div class="form-group">
+              <label for="gigdescription" class="col-sm-2 control-label dis ">Description</label>
+              <div class="col-sm-10">
+                <textarea class="form-control" id="prjdesc" name="prjdesc" placeholder="Describe your gig" row="5" column="10"></textarea>  
+              </div>
+            </div>
+            <h2 class="loginlead" >Would you like the job to be hourly or fixed ?</h2>
+            <div class="form-group">
+              <div class=" col-sm-10 ">
+                <label class="radio-inline">
+                  <input type="radio" name="jobtype[]" id="jobtype"  value="h"  onChange="change_caption('h')"> Hourly
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="jobtype[]" id="jobtype" value="f" checked="checked" onChange="change_caption('f')">Fixed Price
+                </label>                
+              </div>
+            </div>
+            <h2 class="loginlead" id="mlabel">Whats the best fix price you intend to pay ?</h2>
+            <div class="form-group">
+              <div class="col-sm-10">
+                <input  required style="width:30%" type="text" name="proposedprice" id="proposedprice" class="form-control"  placeholder="" onKeyDown="return only_numbers(event);">
+              </div>
+            </div>
+            <h2 class="loginlead" >Skills</h2>
+            <div class="form-group">
+              <div class="col-sm-10">
+                <input  required type="hidden" name="keywords" id="keywords"  class=""    />
+              </div>
+            </div>
+           
+            <h2 class="loginlead">Enter expiry date of your Gig</h2>
+            <div class="form-group">
+              <div class="col-sm-10">
+                <input type="text" id="datepic"style="width:47%" name="enddate" class="mdatepicker"/>
+                
+              </div>
+            </div>
+   					
+                    <?php $tags=get_tags();
+						$tags=implode(",",$tags);
+						?>
+                         
+          <script type="text/javascript">$("#keywords").select2({tags:[<?=$tags;?>]});</script>
+            <h2 class="loginlead"> Would you like to invite any Gigster’s for your project ?</h2>
+            <div class="form-group">
+              <div class="col-sm-10 ">
+                <label class="radio-inline">
+                  <input type="radio" name="inviteusers[]" id="inviteusers"  value="y"> Yes
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="inviteusers[]" id="inviteusers"  value="n"> No
+                </label>                   
+              </div>            
+            </div>
+               <div class="form-group submit">
+         			<button type="submit" class="btn btn-warning">Post Gig</button>
                </div>
-              </div>  
-    
-
-     
-        
-	     </section>
+               </form>
+         </section>
+         &nbsp;
+         <section class="postgigform mhidden" id="inviteform">                       
+         <h2 id="login1">Invite Users</h2>    
+          <h2 class="source">Post a new Gig for free. Invite Gigsters to bid on your gig.</h2>                          
+          <form class="form-horizontal postgigforminner" action="<?php echo $serverpath;?>saveGig" role="form" method="post" target="targetframe" >
+            <div class="form-group">
+              <label for="creategig" class="col-sm-2 control-label labelb">Title</label>
+              <div class="col-sm-10"> 
+                <input type="text" required class="form-control" id="prjTitle" name="prjTitle" placeholder="Looking for a marriage photographer" >
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="gigdescription" class="col-sm-2 control-label dis ">Description</label>
+              <div class="col-sm-10">
+                <textarea class="form-control" id="prjdesc" name="prjdesc" placeholder="Describe your gig" row="5" column="10"></textarea>  
+              </div>
+            </div>
+            <h2 class="loginlead" >Would you like the job to be hourly or fixed ?</h2>
+            <div class="form-group">
+              <div class=" col-sm-10 ">
+                <label class="radio-inline">
+                  <input type="radio" name="jobtype[]" id="jobtype"  value="h"  onChange="change_caption('h')"> Hourly
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="jobtype[]" id="jobtype" value="f" checked="checked" onChange="change_caption('f')">Fixed Price
+                </label>                
+              </div>
+            </div>
+            
+            <h2 class="loginlead" id="mlabel">Whats the best hourly pricing you intend to pay ?</h2>
+            <div class="form-group">
+              <div class="col-sm-10">
+                <input  required style="width:30%" type="text" name="proposedprice" id="proposedprice" class="form-control"  placeholder="">
+              </div>
+            </div>
+           
+            <h2 class="loginlead"> Would you like to invite any Gigster’s for your project ?</h2>
+            <div class="form-group">
+              <div class="col-sm-10 ">
+                <label class="radio-inline">
+                  <input type="radio" name="inviteusers[]" id="inviteusers"  value="y"> Yes
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="inviteusers[]" id="inviteusers"  value="n"> No
+                </label>                   
+              </div>            
+            </div>
+               <div class="form-group submit">
+         			  <button type="submit" class="btn btn-warning">Post Gig</button>
+               </div>
+               </form>
+         </section>
       </div>
     </div>
 </div>
