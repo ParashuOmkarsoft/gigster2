1b22091ba2d1b49fb005ea9343e13941824207c6
saurabhundre <saurabh@fountaintechies.com>
2014-11-10 18:45:20 +0530

    footer margin

--- index.php
+++ index.php
@@ -85,7 +85,10 @@ wild life pictures with us.</p>
 
 <div class="container box-container" style="
     background: #fab518;">
-<div style="text-align: center; margin-top: 50px;"><h2 class="fname3">Why Use Gigster</h2></div>
+<div style="text-align: center; margin-top: 50px;background: #fab518;
+margin: 0;
+max-width: 1349px;
+padding-bottom: 192px;"><h2 class="fname3">Why Use Gigster</h2></div>
 
   <div class="box3">
          <div class="box-1">
