d9bcd0ba86914e2af17dcdcb4c05dad1a0885ed2
saurabhundre <saurabh@fountaintechies.com>
2014-11-10 11:02:39 +0530

    profile margin

--- my-account.php
+++ my-account.php
@@ -31,7 +31,8 @@ include('cfg/more-functions.php');
     <section id="firstsection" class="container">
       <div class="row"style="max-width: 958px;padding-bottom: 15px;">
       <div id="paraprofile">
-          <div class="col-md-6">
+          <div class="col-md-6" style="
+    margin-top: 25px;>
           <?php $nametodisplay=$uInfo['fname'].' '.$uInfo['lname'];
 		  $nametodisplay1=str_replace(" ","",$nametodisplay);
 		  if($nametodisplay1)
@@ -143,7 +144,7 @@ include('cfg/more-functions.php');
   }
 	?>
   </div>
-  <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 150px;"></div>
+  <div class="fileinput-preview fileinput-exists thumbnail" style="width: 200px;margin-right: -26px;"></div>
   <div>
     <span class="btn btn-default btn-file"><span class="fileinput-new">Select image</span><span class="fileinput-exists">Change</span><input type="file" name="profileimage" id="profileimage" /></span>
     <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
@@ -163,7 +164,8 @@ include('cfg/more-functions.php');
             </div>
           </form>  
           
-          <a href='javascript:void(0);' onClick="visible_invisible('frmprofile','paraprofile');" ><img src="<?php echo $serverpath;?>images/pencil.png"></a>     
+          <a href='javascript:void(0);' onClick="visible_invisible('frmprofile','paraprofile');" ><img src="<?php echo $serverpath;?>images/pencil.png"  style="
+    margin-right: -15px;></a>     
       </div>
          
     </section>
