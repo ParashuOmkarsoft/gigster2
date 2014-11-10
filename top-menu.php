01fd52b273e05d7875be9499e069f2613a1b1f0d
saurabhundre <saurabh@fountaintechies.com>
2014-11-10 11:23:51 +0530

    bid btn

--- top-menu.php
+++ top-menu.php
@@ -22,7 +22,7 @@
 					  ?>
                       <li><a href="<?php echo $serverpath;?>allgigs">LIST GIGS</a></li>
                       <li><a href="<?php echo $serverpath;?>gigsters">GIGSTERS</a></li>
-                    <li><a data-toggle="modal" href="#invitemodal">INVITE</a></li>
+                   <!--  <li><a data-toggle="modal" href="#invitemodal">INVITE</a></li> -->
 
 
 
