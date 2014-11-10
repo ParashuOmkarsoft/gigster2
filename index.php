e7edd93894b8895c23020e06bc796be66ab132c9
saurabhundre <saurabh@fountaintechies.com>
2014-11-10 18:37:38 +0530

    home-page

--- index.php
+++ index.php
@@ -20,6 +20,126 @@ include('cfg/more-functions.php');
     <div id="imgback">
       <img src="<?php echo $serverpath;?>images/grooming.png">
     </div>
+
+<div class="container box-container">
+  <div class="box1">
+         <div class="box-1">
+          <span class=""><img src="images/person1.jpg" alt=""  style="padding: 20px;float: left;" class="img-circle"></span>
+          <h2 class="fname">Wild Life Photography </h2>
+          <p>We are looking for a photographerwho can work on aweekend to take wild life pictures with us..</p>
+         </div>   
+  </div>
+
+  <div class="box1">
+         <div class="box-1">
+          <span class="convenience-icon"><img src="images/person3.jpg" alt=""  style="padding: 20px;float: left;" class="img-circle"></span>
+          <h2 class="fname">Need a Carpentar  </h2>
+          <p>We are looking for a photographer
+who can work on aweekend to take 
+wild life pictures with us.</p>
+         </div>   
+  </div>
+
+  <div class="box1">
+         <div class="box-1">
+          <span class="convenience-icon"><img src="images/person2.jpg" alt=""  style="padding: 20px;float: left;" class="img-circle"></span>
+          <h2 class="fname">Need Design Banner  </h2>
+          <p>We are looking for a photographerwho can work on aweekend to take wild life pictures with us..</p>
+         </div>   
+  </div>
+</div>
+  <div style="text-align: center; margin-top: 50px; margin-bottom: 120px;"><button class="btn more-btn" type="submit">MORE GIGS</button></div>
+
+
+
+<div class="container box-container">
+<div style="
+    float: right;
+"><img src="images/lap.png"></div> 
+  <div class="box2">
+         <div class="box-2">
+          <span class=""><img src="images/step1.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
+          <h2 class="fname2">1. Get Anything Done</h2>
+          <p>We are looking for a photographerwho can work on aweekend to take wild life pictures with us..</p>
+         </div>   
+  </div>
+
+  <div class="box2">
+         <div class="box-2">
+          <span class="convenience-icon"><img src="images/step2.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;" ></span>
+          <h2 class="fname2">2. Choose the Gigster</h2>
+          <p>We are looking for a photographer
+who can work on aweekend to take 
+wild life pictures with us.</p>
+         </div>   
+  </div>
+
+  <div class="box2">
+         <div class="box-2">
+          <span class="convenience-icon"><img src="images/step3.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;" ></span>
+          <h2 class="fname2">3. All done! </h2>
+          <p>We are looking for a photographerwho can work on aweekend to take wild life pictures with us..</p>
+         </div>   
+  </div>
+</div>
+
+<div class="container box-container" style="
+    background: #fab518;">
+<div style="text-align: center; margin-top: 50px;"><h2 class="fname3">Why Use Gigster</h2></div>
+
+  <div class="box3">
+         <div class="box-1">
+          
+          <h2 class="fname4">3. Available online and on 
+mobile, making it easy to 
+choose and communicate 
+with your Gigster </h2>
+          
+         </div>   
+  </div>
+
+  <div class="box3">
+         <div class="box-1">
+          
+          <h2 class="fname4">2. No need to spend hours on
+searching, We will find the right
+Gigster for you.</h2>
+         
+         </div>   
+  </div>
+
+
+
+  <div class="box3">
+         <div class="box-1">
+          
+          <h2 class="fname4">1. Get help for anything. 
+Done by your local Gigsters.  </h2>
+          
+         </div>   
+  </div>
+
+  <div class="box3">
+         <div class="box-1">
+          
+          <h2 class="fname4">
+4. There are no service fees! 
+Pay directly to the Gigster.</h2>
+         
+         </div>   
+  </div>
+
+  <div class="box3">
+         <div class="box-1">
+          
+          <h2 class="fname4">
+4. There are no service fees! 
+Pay directly to the Gigster.</h2>
+         
+         </div>   
+  </div>
+
+
 <?php
 include('footer.php'); 
 ?>
