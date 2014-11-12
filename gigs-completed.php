18c172d8e8dc1679eb03a15313dc14dc3ab82abd
amaninfodreams <aman.infodreams@gmail.com>
2014-11-12 15:10:25 +0530

    j48-13
    checking

--- gigs-completed.php
+++ gigs-completed.php
@@ -116,9 +116,7 @@ include('cfg/more-functions.php');
 			   $checkQuery="select a.* from btr_assignment as a ,btr_projects as p where p.userId='".$uId."' and a.projectId=p.prjId and p.status='3'";
 			  $checkSql=@db_query($checkQuery);
 
-			 for($ad=0;$ad<$checkSql['count'];$ad++)
-			 {
-				 $profilepicId = $checkSql['rows'][$ad]['awardedto'];
+				 $profilepicId = $checkSql['rows'][0]['awardedto'];
 		    	$gigsterInfo=get_user_Info(encrypt_str($profilepicId));
 				
 				
@@ -138,7 +136,7 @@ include('cfg/more-functions.php');
                    <div class="tyco"><h4><a <?php /*?>href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"<?php */?>><?php //echo strip_string($nametodisplay,6);?></a></h4></div>
                    <h4>&nbsp;</h4>
               </div>
-            <?php } ?>
+
               
 <!-- end bid model -->
           </div>
