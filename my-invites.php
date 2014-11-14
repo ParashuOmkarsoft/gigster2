24adceb2ae9624bfd7a0797c739fb8e407fe83b3
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-11-14 19:05:07 +0530

    my-invites

--- my-invites.php
+++ my-invites.php
@@ -30,10 +30,45 @@ if($ngigdetails)
 
 ?>
 
-
+<form action="<?php echo $serverpath;?>saveinvites.php" method="post" target="targetframe" onSubmit="return validate_selected();">
+<input type="hidden" name="projectId" id="projectId" value="<?php echo $projectId;?>" />
 <div class="row clearfix">
 		<div class="col-md-12 column">
+        <p ><?php echo $msg;?></p>
+        <?php
+		if($gigsters['count']>0)
+			{
+		?>
 			<div class="row clearfix">
+            <?php
+			for($i=0;$i<$gigsters['count'];$i++)
+				{
+					
+					$gigsterInfo=get_user_Info(encrypt_str($gigsters['rows'][$i]['userId']));
+					if($gigsterInfo['userId']!=$loggedinuser)
+					{
+					$gigsterpic="uploads/profileimage/".$gigsterInfo['profileimage'];
+					
+					if(!empty($gigsterInfo['profileimage']))
+					{
+						$gigsterpic=$gigsterpic;
+					}
+					else
+					{
+						$gigsterpic="images/admin.png";
+					}
+						$gigsternametodisplay="";
+						$gigsternametodisplay=$gigsterInfo['fname'].' '.$gigsterInfo['lname'];
+						$gigsternametodisplay1=str_replace(" ","",$gigsternametodisplay);
+					  if(!$gigsternametodisplay1)
+					  {
+						  $gigsternametodisplay=$gigsterInfo['username'];
+					  }
+					   $gigsterrating=0;
+					$gigsterrating=get_user_rating($gigsterInfo['userId']);
+					
+					
+			?>
 			<div class="col-md-6 column">
 					<div class="row clearfix">
 						<div class="col-md-3 column">
@@ -60,129 +95,13 @@ if($ngigdetails)
 						</div>
 					</div>
 				</div>
-				<div class="col-md-6 column">
+			<div class="col-md-6 column">
 					<div class="row clearfix">
 						<div class="col-md-3 column">
-							<img src="images/person1.jpg">
+							<a href="<?php echo get_profile_link($serverpath,$gigsterInfo['userId']); ?>" target="_blank"><img src="<?php echo $serverpath;?>image.php?image=/<?php echo $gigsterpic;?>&width=75&height=75&cropratio=1:1"></a>
 						</div>
 						<div class="col-md-9 column">
-							
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-							<h4>
-								saurabh undre
-							</h4>
-							<h2 id="map"  style="margin: 0 auto;">
-								singapore
-							</h2>
-							<h4>
-								php,html5
-							</h4>
-							
-							<input type="checkbox" name="invited[]" id="invited" value="1">
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 column">
-					<div class="row clearfix">
-						<div class="col-md-3 column">
-							<img src="images/person1.jpg">
-						</div>
-						<div class="col-md-9 column">
-							
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-							<h4>
-								saurabh undre
-							</h4>
-							<h2 id="map"  style="margin: 0 auto;">
-								singapore
-							</h2>
-							<h4>
-								php,html5
-							</h4>
-							
-							<input type="checkbox" name="invited[]" id="invited" value="1">
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 column">
-					<div class="row clearfix">
-						<div class="col-md-3 column">
-							<img src="images/person1.jpg">
-						</div>
-						<div class="col-md-9 column">
-							
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-								<img src="http://gigster2.fountaintechies.com/images/star_4.png">
-							<h4>
-								saurabh undre
-							</h4>
-							<h2 id="map"  style="margin: 0 auto;">
-								singapore
-							</h2>
-							<h4>
-								php,html5
-							</h4>
-							
-							<input type="checkbox" name="invited[]" id="invited" value="1">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-<form action="<?php echo $serverpath;?>saveinvites.php" method="post" target="targetframe" onSubmit="return validate_selected();">
-<input type="hidden" name="projectId" id="projectId" value="<?php echo $projectId;?>" />
-<p><?php echo $msg;?></p>
-	<?php 
-	if($gigsters['count']>0)
-	{
-		for($i=0;$i<$gigsters['count'];$i++)
-		{
-			$gigsterInfo=get_user_Info(encrypt_str($gigsters['rows'][$i]['userId']));
-			if($gigsterInfo['userId']!=$loggedinuser)
-			{
-			$gigsterpic="uploads/profileimage/".$gigsterInfo['profileimage'];
-			
-			if(!empty($gigsterInfo['profileimage']))
-			{
-				$gigsterpic=$gigsterpic;
-			}
-			else
-			{
-				$gigsterpic="images/admin.png";
-			}
-				$gigsternametodisplay="";
-			    $gigsternametodisplay=$gigsterInfo['fname'].' '.$gigsterInfo['lname'];
-				$gigsternametodisplay1=str_replace(" ","",$gigsternametodisplay);
-			  if(!$gigsternametodisplay1)
-			  {
-				  $gigsternametodisplay=$gigsterInfo['username'];
-			  }
-			   $gigsterrating=0;
-			$gigsterrating=get_user_rating($gigsterInfo['userId']);
-			?>
-	
-			<div class="col-sm-6">
-				<div><a href="<?php echo get_profile_link($serverpath,$gigsterInfo['userId']); ?>" target="_blank"><img src="<?php echo $serverpath;?>image.php?image=/<?php echo $gigsterpic;?>&width=75&height=75&cropratio=1:1"></a>
-				</div>
-                <div>
-                <h4><a href="<?php echo get_profile_link($serverpath,$gigsterInfo['userId']); ?>" target="_blank"><?php echo $gigsternametodisplay; ?></a></h4>
-                <h4><?php echo $gigsterInfo['skills']; ?></h4>
-                <h2 id="map"><?php echo $gigsterInfo['city']; ?></h2>
-                </div>
-                 <?php
+							 <?php
                               
 							   for($t=$gigsterrating;$t<5;$t++)
 							  {
@@ -197,18 +116,29 @@ if($ngigdetails)
         <?php
 							  }
 							  ?>
-                               <div class="clearfix"></div>
-            </div>
-            <div class="clearfix"></div>
-			<?php
-		}
-		}
-	}
-	?>
-	<div><input type="checkbox" name="invited[]" id="invited" value="<?php echo $gigsterInfo['userId'];?>" /></div>
-</div>
-   <!--  <div class="clearfix"></div> -->
-    <button type="submit" class="btn invite-btn" style="margin-top=20px";>Invite Gigsters</button>
-    <button type="submit" class="btn update-btn" data-dismiss="modal" style="margin-top=20px";>Leave It</button>    
-    </form>
-    <div class="clearfix"></div>
+							<h4>
+								<a href="<?php echo get_profile_link($serverpath,$gigsterInfo['userId']); ?>" target="_blank"><?php echo $gigsternametodisplay; ?></a>
+							</h4>
+							<h2 id="map"  style="margin: 0 auto;">
+								<?php echo $gigsterInfo['city']; ?>
+							</h2>
+							<h4>
+								<?php echo $gigsterInfo['skills']; ?>
+							</h4>
+							
+							<input type="checkbox" name="invited[]" id="invited" value="<?php echo $gigsterInfo['userId'];?>">
+						</div>
+					</div>
+
+			</div>
+            <?php
+				}
+			}
+			}
+			?>
+		</div>
+	</div>
+</form>
+
+
+	
