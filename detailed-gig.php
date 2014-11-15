8b38180cd3f763f047c0cf680ac9316e7bb84e96
saurabhundre <saurabh@fountaintechies.com>
2014-11-15 17:15:54 +0530

    undo

--- detailed-gig.php
+++ detailed-gig.php
@@ -57,12 +57,12 @@ else
       <h2 style="font-size:20px;color: #6C3E08;"><strong>Proposed budget - </strong><?php echo $gigdetails['proposedbudget']; ?>&nbsp;<?php echo $currency; ?></h2>
       <h2 id="map"><?php echo $gigsterInfo['city'];?></h2>
     </div>
-   
+    <div id="front" class="col-md-4 giginnerimg">
+      
      
     
      <!--  <div class="col-md-6" style="width: 276px;"> -->
         <div class="col-md-6" style="width: 273px;"> 
-         <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=80&height=80&cropratio=1:1">
         <h2 class="mikename1"><?php echo $nametodisplay;?></h2><br><br><br>
         <?php
 		
