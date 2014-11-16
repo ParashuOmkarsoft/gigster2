e023b438849bf72a9c863585a80fc30d2e90b83a
saurabhundre <saurabh@fountaintechies.com>
2014-11-16 12:53:47 +0530

    cc

--- user-bided.php
+++ user-bided.php
@@ -167,11 +167,15 @@ box-shadow: 0px 0px 2px #000000;'";
         <div class="mike"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=75&height=75&cropratio=1:1"></div>
         <div style="clear:both"></div>
         <div class="tyco"style="width: 169px;">
-          <h4 style="float: right;"><?php echo $nametodisplay; ?></h4>
+          <h4 style="float: right;"><?php echo $nametodisplay; ?></h4><br>
+          <img src="<?php echo $serverpath;?>images/star_4.png" />
+          <img src="<?php echo $serverpath;?>images/star_4.png" />
+          <img src="<?php echo $serverpath;?>images/star_4.png" />
+          <img src="<?php echo $serverpath;?>images/star_4.png" />
           <img src="<?php echo $serverpath;?>images/star_4.png" />
         </div>
         <div style="float: left;width: 200px; margin-left: 126px;">
-        <img src="<?php echo $serverpath;?>images/symbol.png" title="Awarded to some on else" style="cursor:pointer;margin-top: 0;margin-right: 23px;"class="budget"/>Awarded to some on else
+        <img src="<?php echo $serverpath;?>images/symbol.png" title="Awarded to some on else" style="cursor:pointer;margin-top: 0;margin-right: 23px;"/>Awarded to else
         </div>
       </div>
     </div>
