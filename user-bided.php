be9d510c3147e851e8fe30ba780f0f2381c7cf98
saurabhundre <saurabh@fountaintechies.com>
2014-11-17 17:34:35 +0530

    css

--- user-bided.php
+++ user-bided.php
@@ -67,144 +67,252 @@ if(!is_project_awarded_to_user($prjDetails['prjId'],$uInfo['userId']))
 {
 ?>
   <section id="firstsection" class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h3><a href="<?php echo $serverpath;?>gigDetails/<?php echo mera_url_noslash($prjDetails['prjTitle']);?>/<?php echo $prjDetails['prjId'];?>"><?php echo $prjDetails['prjTitle'];?></a></h3>
-      </div>
-      <div class="col-md-12" style="padding:0px;"> 
-      <div class="col-md-8"> 
-       <span class="budget" style="padding-top: 20px;"><?php echo $prjDetails['proposedbudget']; ?> <?php echo $currency ; ?></span>
-       </a> <a href="#msgmodal<?php echo $prjDetails['userId'];?>" data-toggle="modal"><img src="<?=$serverpath;?>images/mail.jpg"></a>
-       <div id="msgmodal<?php echo $prjDetails['userId'];?>" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
-          <div class="modal-dialog modal-lg">
-            <div class="modal-content cform">
-              <div class="container">
-                <div class="col-md-12">
-                  <form class="form-horizontal postgigforminner" action="<?php echo $serverpath;?>sendmessage" target="targetframe" role="form" method="post" >
-                    <input type="hidden" id="fromId" name="fromId" value="<?php echo $uInfo['userId'];?>" />
-                    <input type="hidden" id="toId" name="toId" value="<?php echo $prjDetails['userId'];?>" />
-                    <input type="hidden" id="projectId" name="projectId" value="<?php echo $prjDetails['prjId'];?>" />
-                    <h2 id="login1">Messages</h2>
-                    <h2 class="source"><?php echo $prjDetails['prjTitle'];?></h2>
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label class="col-md-4 control-label tfont">Message</label>
-                        <Br/>
-                        <br/>
-                        <div class="col-md-12">
-                          <textarea name="message" id="message" class="form-control mtextarea" ></textarea>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-10 logsign">
-                          <button type="submit" class="btn btn-warning loginbtn">Send Message</button>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-                <div class="col-md-12" style="height:250px; overflow:scroll; ">
-                  <?php
-			$muInfo=get_user_Info($_SESSION['uId']);
-			$muId=$muInfo['userId'];
-			$other=get_oponent($pId,$muId);
-		 	$projectMessages="select * from btr_messages where projectId=".$prjDetails['prjId']." and (msgfrom=$muId or msgto=$muId) order by msgId DESC";
-			$projectMessages=@db_query($projectMessages);
-			$messages=$projectMessages;
-			for($t=0;$t<$messages['count'];$t++)
-			{
-			$msgfrom=$messages['rows'][$t]['msgfrom'];
-			$fromInfo=get_user_Info(filter_text(encrypt_str($msgfrom)));
-			$fromuserimg=$fromInfo['profileimage'];
-			$buserimage="";
-			if(!$fromuserimg)
-			{
-				$buserimage=filter_text('img/avatar5.png');
-			}
-			else
-			{
-				$buserimage="uploads/profileimage/".$fromuserimg;
-			}
-			if($t%2==0)
-			{
-				$cl="style='background-color:#f8f8f8; margin-top: 3px;padding:5px;border-radius:10px;-moz-box-shadow: 0px 0px 2px #000000;
--webkit-box-shadow: 0px 0px 2px #000000;
-box-shadow: 0px 0px 2px #000000;'";
-			}
-			else
-			{
-				$cl="style='background-color:white;padding:5px;border-radius:10px;-moz-box-shadow: 0px 0px 2px #000000;
--webkit-box-shadow: 0px 0px 2px #000000;
-box-shadow: 0px 0px 2px #000000;'";
-			}
-			//$updatemessage=@db_query("update btr_messages set isread='1' where msgId=".$messages['rows'][$t]['msgId']);	
-			?>
-                  <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="online"/> <br/>
-                    <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small><br/>
-                      <?php echo get_user_name($msgfrom);?> </a><br/>
-                      <?php echo stripslashes(stripslashes(html_entity_decode($messages['rows'][$t]['msgcontent'])));
-					  ?><br/>
-                    </p>
-                  </div>
-                  <br/>
-                  <?php
-		}
-		  ?>
-                </div>
-              </div>
-            </div>
+    <div class="row myrow"  style="border-top: none; border-bottom:none;">
+  <div class="col-md-12" style="border-bottom: 3px solid #fd8900; padding-bottom: 15px;">
+    <div class="col-md-8" style="padding: 0;">
+      <h2 id="giglisth2"><a href="<?php echo $serverpath;?>gigDetails/<?php echo mera_url_noslash($opengig['prjTitle']);?>/<?php echo $opengig['prjId'];?>"><?php echo $opengig['prjTitle'];?></a></h2>
+      <!-- <h2 id="map"><?php echo $gigsterInfo['city'];?></h2> -->
+        <div class="col-md-8" style="padding: 0;">
+        <!-- <p id="gigpara"> -->
+        <h3 class="comp-h4">Feedback</h3>
+         <?php 
+     $mawardedto=project_awarded_to($opengig['prjId']);
+     $userReview="";
+     $userReview=get_project_feedback_1($opengig['prjId'],$opengig['userId']);
+     if($userReview)
+     {
+     ?>
+     <h4>Gig owner</h4><div class="row">
+            <?php 
+            
+      ?>
+             <div class="col-md-10">
+          <div class="col-md-12" style="padding: 0px;">
+      <?php if($userReview['feedback'])
+      {
+        echo $userReview['feedback'];
+      }
+      ?>
+      <Br/>
+      <?php
+      $prjRating=$userReview['rating'];
+        for($t=$prjRating;$t<5;$t++) 
+      {
+       ?>
+            <img src="<?php echo $serverpath;?>images/star_2.png" />
+         <?php
+      }
+       for($t=0;$t<$prjRating;$t++)
+      {
+      ?>
+            <img src="<?php echo $serverpath;?>images/star_1.png" />
+      <?php
+      }
+      ?>
           </div>
-        </div>
+          </div>
+    <?php 
+     }
+      $userReview="";
+     $userReview=get_project_feedback_1($opengig['prjId'],$mawardedto['awardedto']);
+     if($userReview)
+     {
+     ?>
+     <div class="row">
+            <?php 
+            
+      ?>
+          <div class="col-md-10">
+          <div class="col-md-12">
+          <h4>Gigster</h4>
+      <?php if($userReview['feedback'])
+      {
+        echo $userReview['feedback'];
+      }
+      ?>
+      <Br/>
+      <?php
+      $prjRating=$userReview['rating'];
+        for($t=$prjRating;$t<5;$t++) 
+      {
+       ?>
+            <img src="<?php echo $serverpath;?>images/star_2.png" />
+         <?php
+      }
+       for($t=0;$t<$prjRating;$t++)
+      {
+      ?>
+            <img src="<?php echo $serverpath;?>images/star_1.png" />
+      <?php
+      }
+      ?>
+          </div>
+          </div>
+    <?php 
+     }
+    ?>
       </div>
-    <div class="col-md-4"style="margin-top: -30px;">
-        <div class="mike"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=75&height=75&cropratio=1:1"></div>
-        <div style="clear:both"></div>
-        <div class="tyco pull-right";>
-          <h4 style="float: right;"><?php echo $nametodisplay; ?></h4><br>
-          <!-- <div>
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          </div> -->
-        </div>
-        
-      </div>
-      <div style="float: right;">
-        <img src="<?php echo $serverpath;?>images/symbol.png" title="Awarded to some on else" style="cursor:pointer;margin-top: 0;"/>Awarded to else
-        </div>
-    </div>
-    </div>   
-    <div class="row">
-      <div class="col-md-10" style="margin-bottom: 15px;"> 
       
-        <?php echo $prjDetails['prjdesc']; ?>
-
-        <?php
-		if(is_project_awarded($prjDetails['prjId']))
-		{
-			?>
-                    <br/>
-                    <div class="clearfix"></div>
-          
-			
-       
-            </span>
-			<?php
-		}
-		?>
-        
-        </div>
-     
+      </div>
     </div>
-  </section>
-  <?php	
-}
-	   }
-}else
-	{
-		?>
+      <div class="col-md-4"><span id="bid"></span></div>
+     <!--  <div class="col-md-8"><span class="bid">Posted :<?php echo get_time($opengig['postedon']); ?></span></div> -->
+    </div>
+    <div class="col-md-4" style="padding: 0;"> 
+
+        <div class="pull-right" style="padding-top: 20px;/* position: absolute; */float: right;/* margin-top: 132px; *//* padding-left: 0px; */margin-left: 0px;">
+         <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=75&height=75&cropratio=1:1">
+          <h4><?php echo $nametodisplay; ?>Mike</h4>
+          <img src="images/star_1.png" style="float: left;">
+          <img src="images/star_1.png" style="float: left;">
+          <img src="images/star_1.png" style="float: left;">
+          <img src="images/star_1.png" style="float: left;">
+          <img src="images/star_1.png" style="float: left;">
+       </div>               
+ </div>
+</div>
+    
+    
+  <?php
+    }
+    if ($page == 0) $page = 1;          //if no page var is given, default to 1.
+    $prev = $page - 1;              //previous page is page - 1
+    $next = $page + 1;              //next page is page + 1
+    $lastpage = ceil($total_pages/$limit);    //lastpage is = total pages / items per page, rounded up.
+    $lpm1 = $lastpage - 1;
+    $targetpage=$serverpath."allgigs";            //last page minus 1
+    $pagination = "";
+    if($lastpage > 1)
+    {
+    $pagination .= "<div class=\"lastpagination\"><ul class=\"pagination\">";
+    //previous button
+    if ($page > 1)
+      $pagination.= "<li><a href=\"$targetpage/$prev\">« Previous</a></li>";
+    else
+      $pagination.= "<li class=\"disabled\"><a href='#'> Previous</a></li>";
+
+    //pages
+    if ($lastpage < 7 + ($adjacents * 2)) //not enough pages to bother breaking it up
+    {
+      for ($counter = 1; $counter <= $lastpage; $counter++)
+      {
+        if ($counter == $page)
+          $pagination.= "<li class=\"active\"><a href='#'>$counter</a></li>";
+        else
+          $pagination.= "<li><a href=\"$targetpage/$counter\">$counter</a></li>";
+      }
+    }
+    elseif($lastpage > 5 + ($adjacents * 2))  //enough pages to hide some
+    {
+      //close to beginning; only hide later pages
+      if($page < 1 + ($adjacents * 2))
+      {
+        for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<li class=\"active\"><a href='#'>$counter</a></li>";
+          else
+            $pagination.= "<li><a href=\"$targetpage/$counter\">$counter</a></li>";
+        }
+        $pagination.= "...";
+        $pagination.= "<li><a href=\"$targetpage/$lpm1\">$lpm1</a></li>";
+        $pagination.= "<li><a href=\"$targetpage/$lastpage\">$lastpage</a></li>";
+      }
+      //in middle; hide some front and some back
+      elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+      {
+        $pagination.= "<li><a href=\"$targetpage/1\">1</a></li>";
+        $pagination.= "<li><a href=\"$targetpage/2\">2</a></li>";
+        $pagination.= "...";
+        for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<li class=\"active\"><a href='#'>$counter</a></li>";
+          else
+            $pagination.= "<li><a href=\"$targetpage/$counter\">$counter</a></li>";
+        }
+        $pagination.= "...";
+        $pagination.= "<li><a href=\"$targetpage/$lpm1\">$lpm1</a></li>";
+        $pagination.= "<li><a href=\"$targetpage/$lastpage\">$lastpage</a></li>";
+      }
+      //close to end; only hide early pages
+      else
+      {
+        $pagination.= "<li><a href=\"$targetpage/1\">1</a></li>";
+        $pagination.= "<li><a href=\"$targetpage/2\">2</a></li>";
+        $pagination.= "...";
+        for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+        {
+          if ($counter == $page)
+            $pagination.= "<li class=\"active\"><a href='#'>$counter</a></li>";
+          else
+            $pagination.= "<li><a href=\"$targetpage/$counter\">$counter</a></li>";
+        }
+      }
+    }
+    //next button
+    if ($page < $counter - 1)
+      $pagination.= "<li><a href=\"$targetpage/$next\">Next</a></li>";
+    else
+      $pagination.= "<li class=\"disabled\"><a href='#'>Next</a></li>";
+    $pagination.= "</ul></div>";
+  }
+?>
+<!-- <div class="col-md-2 giginnerimg gigimg">
+        <?php 
+      $checkQuery="select a.* from btr_assignment as a ,btr_projects as p where p.userId='".$uId."' and a.projectId='".$opengig['prjId']."' and p.status='3' group by a.projectId";
+      $checkSql=@db_query($checkQuery);
+      
+      
+      
+      for($ad=0;$ad<$checkSql['count'];$ad++)
+      {
+      $profilepicId = $checkSql['rows'][$ad]['awardedto'];
+        $gigsterInfo=get_user_Info(encrypt_str($profilepicId));
+      $profilepic="uploads/profileimage/".$gigsterInfo['profileimage'];
+      if(file_exists($profilepic))
+      {
+        $profilepic=$profilepic;
+      }
+      else
+      {
+        $profilepic="images/admin.png";
+      }
+        ?>
+        <div class="col-md-12"> <a href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=45&height=45&cropratio=1:1"></a>
+          <div class="tyco">
+            <h4><a <?php /*?>href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"<?php */?>>
+              <?php //echo strip_string($nametodisplay,6);?>
+              </a></h4>
+             
+    
+          </div>
+          
+          <h4>&nbsp;</h4>
+        </div>
+        <div class="col-md-12">
+              <?Php   
+                $gigsterrating=0;
+              $gigsterrating=get_user_rating($profilepicId); 
+                             
+                 for($t=$gigsterrating;$t<5;$t++)
+                {
+                  ?>
+                  <img src="<?php echo $serverpath;?>images/star_4.png" />
+                  <?php
+                }
+                 for($t=0;$t<$gigsterrating;$t++)
+                {
+                  ?>
+                  <img src="<?php echo $serverpath;?>images/star_3.png" />
+                  <?php
+                }
+                ?>
+                   
+             </div>
+        <?php } ?>
+  </div> -->
+ </div><!-- end of col-12 --> 
   <div class="clearfix"></div>
   <br/>
   <p class="mandatory">Sorry, No Bids submited by you yet.</p>
---
+++ user-completed - Copy.php
@@ -0,0 +1,232 @@
+<?php 
+include('cfg/cfg.php'); 
+include('cfg/functions.php');
+include('cfg/more-functions.php'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $sitename;?>- Completed gigs</title>
+	<?php include('script-header.php'); ?>
+    <?php include('fb-login.php'); ?>
+  </head>
+  <body>
+    <?php include('top-menu.php'); ?>
+    <div id="grad"></div>
+         
+  
+<section class="container">
+<ul id="profilemenu">
+  <li><a href="<?php echo $serverpath;?>assignments">
+   In progress
+    </a></li>
+  <li><a href="<?php echo $serverpath;?>bided"> My Bids </a></li>
+  <li><a href="<?php echo $serverpath;?>tocompleted"><strong>My Completed</strong></a></li>
+</ul>
+      <h2 id="logingigster1">Completed Gigs</h2>
+<?php
+
+
+$mUid=$_SESSION['uId'];
+$muInfo=get_user_Info($mUid);
+$mUid=filter_text($muInfo['userId']);
+$checkQuery="select a.* from btr_assignment as a ,btr_projects as p where  a.awardedto=".$uInfo['userId']." and a.projectId=p.prjId and p.status='3' order by a.completiondate DESC ";
+$checkSql=@db_query($checkQuery);
+if($checkSql['count']>0)
+{
+	 $sno=1;
+     for($i=0;$i<$checkSql['count'];$i++)
+       {
+
+		  $prjDetails=get_gig_details($checkSql['rows'][$i]['projectId']);
+		    $muInfo=get_user_Info(encrypt_str($prjDetails['userId']));
+		  $mId=encrypt_str($checkSql['rows']['0']['id']);
+		  $awardedto=$checkSql['rows']['0']['awardedto'];
+			$nametodisplay="";
+			  $nametodisplay=$muInfo['fname'].' '.$muInfo['lname'];
+			  $nametodisplay1=str_replace(" ","",$nametodisplay);
+			  if(!$nametodisplay1)
+			  {
+				  $nametodisplay=$muInfo['username'];
+			  }
+			  $gigsterrating=0;
+			
+			$profilepic="uploads/profileimage/".$muInfo['profileimage'];
+			
+			if(file_exists($profilepic))
+			{
+				$profilepic=$profilepic;
+			}
+			else
+			{
+				$profilepic="images/admin.png";
+			}
+?>
+<section id="firstsection" class="container">
+      <div class="row">
+          <div class="col-md-8">
+            <h3><a href="<?php echo $serverpath;?>gigDetails/<?php echo mera_url_noslash($prjDetails['prjTitle']);?>/<?php echo $prjDetails['prjId'];?>"><?php echo $prjDetails['prjTitle'];?></a></h3> 
+      </div>
+      <div class="col-md-4">
+				<?php $projectstatus=get_status_details($prjDetails['prjId'],$uInfo['userId']); 
+				
+			  if($projectstatus == '100') { 
+			  if(!is_feedback_given($prjDetails['prjId'],$uInfo['userId']))
+			  {
+			  ?><a href="#statusmodal<?php echo $prjDetails['prjId'];?>" data-toggle="modal">
+        <button type="button" class="btn markascomplete-btn1" >Send feedback</button>
+        </a>
+        
+        <div id="statusmodal<?php echo $prjDetails['prjId'];?>" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
+        <div class="modal-dialog modal-lg"style="max-width: 500px;">
+          <div class="modal-content cform">
+            <div class="container">
+              <div class="col-md-12" style="padding: 0px;">
+                <form class="form-horizontal postgigforminner" action="<?php echo $serverpath;?>finalrating" role="form" method="post">
+                  <input type="hidden" id="projectId" name="projectId" value="<?php echo $prjDetails['prjId'];?>" />
+
+                  <h2 id="login1">Mark gig as complete</h2>
+                  <h2 class="source"><?php echo $opengig['prjTitle'];?></h2>
+                  <div class="col-md-12" style="padding: 0px;">
+                    <div class="form-group">
+                      <label for="inputText" class="col-sm-6 control-label newlog" style="margin-bottom: 30px;">Feedback for gigster</label>
+                     
+                      <div class="col-sm-12">
+                        <textarea class="form-control tinpute mtextarea" placeholder="Your Message" row="10" column="10" required name="experience" id="experience"></textarea>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label class="col-md-2 control-label tfont" style="margin-bottom: 20px;">Rating</label>
+                      
+                      <div class="col-md-10" style="padding: 0px; margin-top:-7px">
+                      
+                        <div class="form-control form-radio" >
+ <input id="input-21d" name="rating" value="<?php echo $rt;?>" type="number" class="rating" min=0 max=5 step=0.5 data-size="sm">
+						
+                         <script type="text/javascript">
+						   $('.rating').rating({'showCaption':true, 'stars':'5', 'min':'0', 'max':'5', 'step':'1', 'size':'xs'});
+						 </script>                        </div>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="col-sm-12 logsign"style="padding: 0px;">
+                        <button type="submit" class="btn mark-btn">Mark as Complete</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>     
+      </div>
+        
+         <?php 
+			  }
+		 }
+		  ?>
+      </div>          
+      </div>
+     <div class="row">
+         <h3>Feedback</h3>
+          	<?php if(get_project_feedback_1($prjDetails['prjId'],$prjDetails['userId']))
+			{
+				?>
+            <h4>Gig owner</h4><div class="row">
+            <?php $userReview=get_project_feedback_1($prjDetails['prjId'],$prjDetails['userId']);
+						
+			?>
+             <div class="col-md-10">
+          <div class="col-md-12">
+		 	<?php if($userReview['feedback'])
+			{
+				echo $userReview['feedback'];
+			}
+			?>
+			<Br/>
+			<?php
+			$prjRating=$userReview['rating'];
+		    for($t=$prjRating;$t<5;$t++) 
+			{
+			 ?>
+		        <img src="<?php echo $serverpath;?>images/star_2.png" />
+		     <?php
+			}
+		   for($t=0;$t<$prjRating;$t++)
+			{
+			?>
+		        <img src="<?php echo $serverpath;?>images/star_1.png" />
+			<?php
+			}
+			?>
+          </div>
+          </div>
+          <?php 
+			}
+			 $mawardedto=project_awarded_to($prjDetails['prjId']);
+			if(get_project_feedback_1($prjDetails['prjId'],$mawardedto['awardedto']))
+			{
+					?>
+            <h4>Gigster</h4><div class="row">
+            <?php $userReview=get_project_feedback_1($prjDetails['prjId'],$mawardedto['awardedto']);
+						
+			?>
+              <div class="col-md-10">
+          <div class="col-md-12">
+		 	<?php if($userReview['feedback'])
+			{
+				echo $userReview['feedback'];
+			}
+			?>
+			<Br/>
+			<?php
+			$prjRating=$userReview['rating'];
+		    for($t=$prjRating;$t<5;$t++) 
+			{
+			 ?>
+		        <img src="<?php echo $serverpath;?>images/star_2.png" />
+		     <?php
+			}
+		   for($t=0;$t<$prjRating;$t++)
+			{
+			?>
+		        <img src="<?php echo $serverpath;?>images/star_1.png" />
+			<?php
+			}
+			?>
+          </div>
+            </div>
+          <?php 
+			}
+		  ?>
+
+  
+        <div class="col-md-2">
+           <div class="mike"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=75&height=75&cropratio=1:1"></div><div class="tyco"><h4><?php echo $nametodisplay; ?></h4>
+        </div>
+       </div>          
+    </div>
+
+
+
+</section>
+
+<?php	
+	   }
+}else
+	{
+		?>
+        <div class="clearfix"></div>
+        <br/>
+		<p class="mandatory">Sorry, No Gigs assigned to you yet.</p>
+                 <br/>
+                <div class="clearfix"></div>
+		<?php
+	}
+?>  </section>
+    <?php include('footer.php'); ?>
+  </body>
+</html>
