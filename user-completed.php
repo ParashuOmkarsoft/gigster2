6a13fb7d25b053816bfdb8a94d8603c49b95e6e7
saurabhundre <saurabh@fountaintechies.com>
2014-11-17 17:56:18 +0530

    star

--- user-completed.php
+++ user-completed.php
@@ -184,6 +184,7 @@ if($checkSql['count']>0)
 			}
 			?>
 			<Br/>
+			<div class="pull-left">
 			<?php
 			$prjRating=$userReview['rating'];
 		    for($t=$prjRating;$t<5;$t++) 
@@ -199,6 +200,7 @@ if($checkSql['count']>0)
 			<?php
 			}
 			?>
+			</div>
           </div>
           </div>
           <?php 
