06752488aac1fcb16e97575808d0f4f70d17cf0f
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-11-19 13:51:16 +0530

    j93

--- my-gigs-assigned.php
+++ my-gigs-assigned.php
@@ -12,6 +12,7 @@ if(!isset($_SESSION['uId']))
 	die();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -146,7 +147,7 @@ if($checkSql['count']>0)
 			  }
 		 }
 		  ?>
-          <a href="#msgmodal<?php echo $gigdetails['userId'];?>" data-toggle="modal"><img src="<?=$serverpath;?>images/mail.jpg"></a>
+          
         
         <div id="msgmodal<?php echo $gigdetails['userId'];?>" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
           <div class="modal-dialog modal-lg">
@@ -376,24 +377,33 @@ $puname=$puuinfo['username'];
         <div style="clear:both"></div>
         <div class="pull-right";>
           <h4 style="float: right;"><?php echo $nametodisplay; ?></h4><br>
-          <!-- <div>
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          <img src="<?php echo $serverpath;?>images/star_1.png" />
-          </div> -->
+          
         </div>
-        
+       
       </div>
+     <?php $ownerrating=get_user_rating($prjDetails['userId']); 
+	 ?>
       <div style="float: right;">
-        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px;margin-right: 15px;"/>
-        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px;"/>
-        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px;"/>
-        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px;"/>
-        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px;"/>
+         <?php
+				 for($t=0;$t<$ownerrating;$t++)
+							  {
+								  ?>
+        <img src="<?php echo $serverpath;?>images/star_1.png" style="margin: 0px 0px 1px 0px;"/>
+        <?php
+							  }
+							   for($t=$ownerrating;$t<5;$t++)
+							  {
+								  ?>
+        <img src="<?php echo $serverpath;?>images/star_2.png" style="margin: 0px 0px 1px 0px;"/>
+        <?php
+							  }
+		?>
         </div>
-    </div>           
+          
+    </div>   
+    <div style="float: right;padding-left:30px;" >
+          <a href="#msgmodal<?php echo $gigdetails['userId'];?>" data-toggle="modal"><img src="<?=$serverpath;?>images/mail.jpg"></a>
+           </div>        
  </div>
     </div>
     <div class="row">
