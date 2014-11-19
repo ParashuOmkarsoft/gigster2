e3d44b0d060f52949d5b1375454d48e359674196
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-11-19 11:19:28 +0530

    j88

--- bid-modal.php
+++ bid-modal.php
@@ -1,5 +1,5 @@
 <div id="bidmodal" class="modal fade  bs-example-modal-lg modelw " tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true" >
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg" style="width:500px !important;">
       <div class="modal-content" id="bid-modal">
         
          &nbsp;
