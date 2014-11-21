161462aadfea14f140648dc2482eb7de6e26864a
saurabhundre <saurabh@fountaintechies.com>
2014-11-21 10:16:08 +0530

    seach-gig

--- search-gig.php
+++ search-gig.php
@@ -18,9 +18,9 @@ include('cfg/more-functions.php');
 
 <?php $term = $_REQUEST['gigsearch']; ?>
 <section class="container">
-  <form class="navbar-form navbar-right" role="search" action="<?php echo $serverpath;?>searchgig" method="post">
-    <div class="form-group"><input type="text" class="form-control" placeholder="Search" name="gigname" value="<?php echo $term; ?>"></div>
-    <button type="submit" class="btn btn-default">Submit</button>
+  <form class="navbar-form navbar-right" role="search" action="<?php echo $serverpath;?>searchgig" method="post" style="padding:0px;">
+    <div class="form-group"><input type="text" class="form-control" placeholder="Search" name="gigname" value="<?php echo $term; ?>" style="border: 1px solid #fab518;border-radius: none;border-radius: 0px;"></div>
+    <img src="images/search.png">
   </form>
   <h2 id="logingigster1">Open Gigs</h2>
   <?php
