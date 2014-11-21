5f0467cf2845147c2d9d0f50b59e82edd190eab6
saurabhundre <saurabh@fountaintechies.com>
2014-11-21 09:54:29 +0530

    search

--- giglist.php
+++ giglist.php
@@ -19,8 +19,9 @@ include('cfg/more-functions.php');
 
 <section class="container">
   <form class="navbar-form navbar-right" role="search" action="<?php echo $serverpath;?>searchgig" method="post">
-          <div class="form-group"><input type="text" class="form-control" placeholder="Search" name="gigsearch"></div>
-          <button type="submit" class="btn btn-default">Submit</button>
+          <div class="form-group"><input type="text" class="form-control" placeholder="Search" name="gigsearch" style="border: 1px solid #fab518;border-radius: none;border-radius: 0px;"></div>
+          <!-- <button type="submit" class="btn btn-default">Submit</button> -->
+          <img src="images/search.png">
         </form>
   <h2 id="logingigster1">Open Gigs</h2>
   <?php
