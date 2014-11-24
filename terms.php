77db4498797181e89e8caa168d40a9738b40c5b0
saurabhundre <saurabh@fountaintechies.com>
2014-11-24 15:45:40 +0530

    terms

--- terms.php
+++ terms.php
@@ -51,29 +51,56 @@ include('cfg/more-functions.php');
          <p class="james">● PUBLISHER: THE PLATFORM GIGSTERGO.COM</p>
          <p class="james">● MARKET: THE MARKET THAT IS TARGETED BY GIGSTERGO.COM IS SINGAPORE.</p>
          <p class="james">● PLATFORM: THE PLATFORM GIGSTERGO.COM REFERS TO THE ONLINE PLATFORM WHICH ALLOWS GIG OWNERS TO POST GIGS AND, FOR GIGSTERS TO RESPOND WITH OFFER TO PROVIDE A SERVICE, A PIECE OF WORK OR AN OBJECT
-<br>● GIGSTERGO.COM: REFERS TO THE PLATFORM GIGSTERGO.COM
-<br>● USER: USER IS A GENERIC TERM REFERRING PRIMARILY TO THE THREE CATEGORIES INDICATED BELOW WHO HAVE ACCESS TO THE PLATFORM: “GIGSTER, VISITOR, AND GIG OWNER”
-<br>● DOWNLOADING RIGHTS (DR) : THE PLATFORM GIGSTERGO.COM OFFERS ITS CLIENTS THE OPPORTUNITY TO DOWNLOAD FILES ACCORDING TO THE TERMS OF THE CONTRACT.
-<br>● INFORMATION OR DATA: EVERY DOCUMENT PUBLISHED ON THE PLATFORM BY THE CLIENT.
-<br>● USERNAME: THE TERM “USERNAME” COVERS ALL INFORMATION NECESSARY TO IDENTIFY A USER ON THE PLATFORM AND TO ALLOW THEM ACCESS TO MEMBER-ONLY AREAS.
-<br>● PASSWORD: THE “PASSWORD“ IS A CONFIDENTIAL PIECE OF INFORMATION WHICH THE USER MUST KEEP SECRET; ALONG WITH THE USERNAME, IT ALLOWS THE USER TO PROVE THEIR IDENTITY.
+<br><br>● GIGSTERGO.COM: REFERS TO THE PLATFORM GIGSTERGO.COM
+<br><br>● USER: USER IS A GENERIC TERM REFERRING PRIMARILY TO THE THREE CATEGORIES INDICATED BELOW WHO HAVE ACCESS TO THE PLATFORM: “GIGSTER, VISITOR, AND GIG OWNER”
+<br><br>● DOWNLOADING RIGHTS (DR) : THE PLATFORM GIGSTERGO.COM OFFERS ITS CLIENTS THE OPPORTUNITY TO DOWNLOAD FILES ACCORDING TO THE TERMS OF THE CONTRACT.
+<br><br>● INFORMATION OR DATA: EVERY DOCUMENT PUBLISHED ON THE PLATFORM BY THE CLIENT.
+<br><br>● USERNAME: THE TERM “USERNAME” COVERS ALL INFORMATION NECESSARY TO IDENTIFY A USER ON THE PLATFORM AND TO ALLOW THEM ACCESS TO MEMBER-ONLY AREAS.
+<br><br>● PASSWORD: THE “PASSWORD“ IS A CONFIDENTIAL PIECE OF INFORMATION WHICH THE USER MUST KEEP SECRET; ALONG WITH THE USERNAME, IT ALLOWS THE USER TO PROVE THEIR IDENTITY.
 </p>
        
 
+    </div>
+    <div class="clearfirst"></div>
+    </div>
+    <div class="row secondinner">
+        <div class="col-md-12">
+         <span id="bond">PURPOSE AND AREAS OF APPLICATION</span> 
+               <p class="james">The PLATFORM GIGSTERGO.COM is open to any company or corporation or individual, who in the future, intends to conclude a service contract with a GIGSTER.<br><br>
+      To this end, the PLATFORM allows GIGSTERS in accordance with these T&Cs, to publish any financial and commercial information relevant to their field of expertise. The PLATFORM also allows GIG OWNERS, in accordance with these T&Cs, access to the information of some of the GIGSTERS.<br><br>
+      The T&Cs apply to any use of the PLATFORM.<br><br>
+      These T&Cs came into force on (01 November 2014) and this version of them cancels and replaces any previous versions or contracts.<br><br>
+      The USER is asked to carefully read the T&Cs, which are accessible via an online link on the PLATFORM. The USER is advised to download and/or print out the T&Cs and to keep a copy for themselves. The USER is also advised to read the T&Cs each time they visit the PLATFORM as they may be modified at any time.<br><br>
+      Any modification to the T&Cs will be notified to the USER by means of a window showing a message to this effect which will appear on the PLATFORM. Any modification of the T&Cs will be deemed to have been tacitly accepted by the USER if the latter continues to connect to the PLATFORM without expressly stating otherwise.<br><br>
+      </p> 
         </div>
          <div class="clearfirst"></div>
-      </div>
-       <div class="row secondinner">
-        <div class="col-md-1">
-          <img src="<?php echo $serverpath;?>images/4.jpg" class="img-circle">
-        </div>
-        <div class="col-md-11">
-         <span id="bond">Why should I post a Gig?</span> 
-         <p class="james">Instead of spending hours finding the right help, post a Gig and we will find the a match for you.</p> 
+    </div>
+    <div class="row secondinner">
+        <div class="col-md-12">
+         <span id="bond">USE OF THE PLATFORM</span> 
+               <p class="james">TUse of the PLATFORM is reserved for USERS who have previously been made aware of, and have accepted the T&Cs by way of ‘clicking the mouse’.<br><br>
+                USERS agree that the content of any messages and documents sent are not contrary to the legislation in force and do not constitute forgery or falsification of documents. They shall undertake not to use the PLATFORM for the purpose of committing a criminal offence or offences.<br><br>
+                Any document or information posted on the PLATFORM which contravenes these provisions will result in the immediate and full termination of these terms and conditions and the exclusion of the USER from the PLATFORM, without prejudicing legal action for any damage caused to GIGSTERGO.COM and/or any of its customers.<br><br>
+                Access to certain parts of the PLATFORM requires a username and password to be entered. The password, chosen by the USER, is personal and confidential.<br><br>
+                The USER is solely responsible for any use of their username and password, and promises to keep the password a secret and to not divulge it in any manner. The use of the username and password over the internet is made at the USER’s own risk. It is their responsibility to take any measures necessary to protect their data against any infringement.<br><br></p> 
         </div>
          <div class="clearfirst"></div>
-      </div>
-
+    </div>
+    <div class="row secondinner">
+        <div class="col-md-12">
+         <span id="bond">ACCESS TO THE SERVICE</span> 
+               <p class="james">The service is available, according to the terms as described in these T&Cs, to every USER who has access to the internet, provided they have expressly accepted these terms and conditions of use.<br><br>
+All costs relating to accessing the Service, are payable exclusively by the USER. They are solely responsible for the smooth running of their IT equipment as well as any access to the internet.<br><br>
+Certain areas of the platform are reserved for USERS who have a username and password.<br><br>
+GIGSTERGO.COM reserves the right to refuse unilaterally the access to the Service, without prior warning, to any USER who does not adhere to the present Terms and Conditions of Use.<br><br>
+GIGSTERGO.COM will make use of all reasonable means at its disposal to ensure quality of access to the Service, but is under no obligation to do so.<br><br>
+GIGSTERGO.COM cannot, in addition, be held responsible for any network or server malfunctions, or for any other event beyond its reasonable control which may prevent access to, or lower the quality of access to the Service.<br><br>
+GIGSTERGO.COM reserves the right to interrupt, momentarily suspend, or modify without prior notice, access to all or part of the Service in order to carry out maintenance tasks or for any other reason, without there being any liability or right to compensation.<br><br>
+<br><br></p> 
+        </div>
+         <div class="clearfirst"></div>
+    </div>
     </section>
 
     <?php
