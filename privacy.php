756fa34f57e43c0a1732dc5184f725b76049a9ca
saurabhundre <saurabh@fountaintechies.com>
2014-11-24 17:05:47 +0530

    strong

---
+++ privacy.php
@@ -0,0 +1,74 @@
+<?php 
+include('cfg/cfg.php'); 
+include('cfg/functions.php');
+include('cfg/more-functions.php'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title><?php echo $sitename;?></title>
+	<?php include('script-header.php'); ?>
+    <?php include('fb-login.php'); ?>
+  </head>
+  <body>
+  <?php include('top-menu.php'); ?>  
+    
+      <section class="container">
+      <h2 id="giglog">Privacy</h2>
+      
+<div class="row firstdinner">
+       
+<div class="col-md-12">
+<span id="bond">Privacy</span> 
+<p class="james">USERS’ privacy are important to us. GIGSTERGO.COM’s Personal Data Protection Policy sets out GIGSTERGO.COM will comply with the provisions of the Personal Data Protection Act 2012 of Singapore (“PDPA”).<br><br>
+GIGSTERGO.COM also want USERS to understand the way in which we collect, use, disclose and/or retain your Personal Data. This Personal Data Protection Policy sets out:<br><br>
+our policies on how we manage your Personal Data;<br><br>
+the types of Personal Data we collect, use, disclose and/ or retain;<br><br>
+how we collect, use, disclose and/ or retain your Personal Data; and<br><br>
+the purpose(s) for which we collect, use, disclose and/ or retain your Personal Data.<br><br>
+USERS agree and consent to us to collect, use and disclose and/or retain your Personal Data in the manner set forth in this Personal Data Protection Policy.<br><br>
+This Personal Data Protection Policy supplements but does not supersede nor replace any other consent which you may have previously provided to us nor does it affect any right that we may have at law in connection with the collection, use, disclosure and/ or retention of your Personal Data.<br><br>
+From time to time, we may update this Personal Data Protection Policy to ensure that our Policy is consistent with developments and/or any regulatory changes. Subject to your legal rights in Singapore, the prevailing terms of the Personal Data Protection Policy shall apply.<br><br>
+
+This Personal Data Protection Policy forms a part of the terms and conditions governing your relationship with us and should be read in conjunction with such T&Cs. In the event of any inconsistency between the provisions of the Personal Data Protection Policy and the T&Cs, the provisions of the T&Cs shall prevail.<br><br>
+</p> 
+</div>
+<div class="clearfirst"></div>
+</div> 
+
+<div class="col-md-12">
+<span id="bond">1.Your Personal Data</span> 
+<p class="james">In this Personal Data Protection Policy, “Personal Data” refers to any data and/or information about you from which you can be identified by, either (a) from that data; or (b) from that data and other information to which we may have legitimate access to. Examples of such Personal Data include but are not limited to:<br><br>
+your name, NRIC, passport or other identification number, telephone number(s), mailing address, email address and any other information relating to you which you have provided in any forms you may have submitted to use, or in other forms of interaction with you;<br><br>
+your photos;<br><br>
+your employment history, education background, and income levels;<br><br>
+Personal Data of your family members, such as next of kin, spouses, and children;<br><br>
+information relating to payments, such as your bank account number or credit card information;<br><br>
+information about your usage of and interaction with our website and/or services including computer and connection information, device capability, bandwidth, statistics on page views and traffic to and from our website<br><br>
+<br><br>
+</p> 
+</div>
+<div class="clearfirst"></div>
+</div>   
+      
+
+    </section>
+
+    <?php
+include('footer.php'); 
+?>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
