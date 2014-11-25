1ae03ac854aa7aa04cf92be6ee65c40e2c6ee73f
saurabhundre <saurabh@fountaintechies.com>
2014-11-25 17:31:50 +0530

    js

--- my-account.php
+++ my-account.php
@@ -349,31 +349,6 @@ include('cfg/more-functions.php');
 <?php include('footer.php'); ?>
   </body>
 
-<script type="text/javascript">
-  $('.btn-toggle').click(function() {
-    $(this).find('.btn').toggleClass('active');  
-    
-    if ($(this).find('.btn-primary').size()>0) {
-      $(this).find('.btn').toggleClass('btn-primary');
-    }
-    if ($(this).find('.btn-danger').size()>0) {
-      $(this).find('.btn').toggleClass('btn-danger');
-    }
-    if ($(this).find('.btn-success').size()>0) {
-      $(this).find('.btn').toggleClass('btn-success');
-    }
-    if ($(this).find('.btn-info').size()>0) {
-      $(this).find('.btn').toggleClass('btn-info');
-    }
-    
-    $(this).find('.btn').toggleClass('btn-default');
-       
-});
 
-$('form').submit(function(){
-  alert($(this["options"]).val());
-    return false;
-});
-</script>
 
 </html>
