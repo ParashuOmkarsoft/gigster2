9e74229f024901dcc3dc66c5e45e61a9c27626dc
saurabhundre <saurabh@fountaintechies.com>
2014-11-25 11:16:59 +0530

    index

--- index.php
+++ index.php
@@ -32,11 +32,79 @@ include('cfg/more-functions.php');
     <div class="col-md-12 column">
       <h2 class="name-what-gigster1 text-center">
        It’s a service that connects the people who need help, with the people who can help.
-       Gigster is all about enabling you to live, work and play better. 
+       <br>Gigster is all about enabling you to live, work and play better. 
       </h2>
     </div>    
   </div>
   </div>
+
+  <div class="row rowbg">
+<div class="container">
+  <div class="row clearfix">
+    <div class="col-md-12"style="padding: 50px 0px 50px 0px;">
+      <h2 class="name2 text-center">
+       Why Use Gigster
+      </h3>
+    </div>
+  </div>
+  <div class="row clearfix">
+    <div class="col-md-4">
+      <h2 class="name1">
+         Get help for anything. Done by your local Gigsters.
+      </h2>
+    </div>
+    <div class="col-md-4">
+      <h2 class="name1 ">
+        No need to spend hours on searching, We will find the right Gigster for you.
+      </h2>
+    </div>
+    <div class="col-md-4">
+      <h2 class="name1 ">
+       Available online and on mobile, making it easy to choose and communicate with your Gigster
+      </h2>
+    </div>
+    <div class="col-md-6">
+      <h2 class="name1 ">
+      No need to spend hours on searching, We will find the right Gigster for you.
+      </h2>
+    </div>
+    <div class="col-md-6">
+      <h2 class="name1 ">
+      Available online and on mobile, making it easy to choose and communicate with your Gigster
+      </h2>
+    </div>
+  </div>
+</div>
+</div>
+
+<div style="text-align: center;background: rgb(225, 225, 225);padding: 16px;padding-top: 0px;"><a href="<?php echo $serverpath;?>allgigs"><button class="btn more-btn" type="submit">MORE GIGS</button></a><hr class="hr"></div>
+<div class="container" style="max-width: 1178px;margin-top: 45px;">
+      <div class="col-md-12 column" style="margin-bottom: 50px;">
+      <h2 class="name-what-gigster text-center">
+       How It Works
+      </h2>
+      </div>
+  
+      <div class="box-1">
+        <span class=""><img src="images/step1.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
+        <h2 class="fname2">Get Anything Done </h2>
+        <p>Tell us what you need help with and post a Gig.</h2>
+      </div>
+      <div class="box-1">
+        <span class=""><img src="images/step2.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
+        <h2 class="fname2">Choose the Gigster </h2>
+        <p>We'll find the right local Gigsters and you just select one.</p>
+      </div>   
+      <div class="box-1">
+        <span class=""><img src="images/step3.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
+        <h2 class="fname2">All done!</h2>
+        <p>Once the Gig is done, you pay directly to the Gigster. No additional charges!</p>
+      </div>   
+  <div style="text-align: center;padding: 16px;padding-top: 0px;"><a href="<?php echo $serverpath;?>allgigs"><button class="btn signup-btn" type="submit">Free Signup!</button></a><hr class="hr"></div>  
+
+</div>
+
+
 
  	<?php $homelatest = latest_gigs();
 	/*if(!empty($homelatest)){
@@ -52,7 +120,7 @@ include('cfg/more-functions.php');
   <?php }}*/ ?>
 
 
-<div class="container" style="max-width: 1349px;background: rgb(225, 225, 225);margin: 0 auto;padding: 50px;">
+<div class="container" style="max-width: 1700px;background: rgb(225, 225, 225);margin: 0 auto;padding: 50px;">
   <?php
   foreach($homelatest['rows'] as $hmltst3 )
 {
@@ -88,70 +156,6 @@ if (strlen($string) > 140) {
   </div>
 </div>
 
-<div style="text-align: center;background: rgb(225, 225, 225);padding: 16px;padding-top: 0px;"><a href="<?php echo $serverpath;?>allgigs"><button class="btn more-btn" type="submit">MORE GIGS</button></a><hr class="hr"></div>
-<div class="container" style="max-width: 1178px;margin-top: 45px;">
-      <div class="col-md-12 column" style="margin-bottom: 50px;">
-      <h2 class="name-what-gigster text-center">
-       How It Works
-      </h2>
-      </div>
-  
-      <div class="box-1">
-        <span class=""><img src="images/step1.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
-        <h2 class="fname2">Get Anything Done </h2>
-        <p>Tell us what you need help with and post a Gig.</h2>
-      </div>
-      <div class="box-1">
-        <span class=""><img src="images/step2.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
-        <h2 class="fname2">Choose the Gigster </h2>
-        <p>We'll find the right local Gigsters and you just select one.</p>
-      </div>   
-      <div class="box-1">
-        <span class=""><img src="images/step3.png" alt=""  style="padding: 20px;float: left;padding-top: 0px;"></span>
-        <h2 class="fname2">All done!</h2>
-        <p>Once the Gig is done, you pay directly to the Gigster. No additional charges!</p>
-      </div>   
-  <div style="text-align: center;padding: 16px;padding-top: 0px;"><a href="<?php echo $serverpath;?>allgigs"><button class="btn signup-btn" type="submit">Free Signup!</button></a><hr class="hr"></div>  
-
-</div>
-<div class="row rowbg">
-<div class="container">
-  <div class="row clearfix">
-    <div class="col-md-12"style="padding-bottom: 50px;">
-      <h2 class="name2 text-center">
-       Why Use Gigster
-      </h3>
-    </div>
-  </div>
-  <div class="row clearfix">
-    <div class="col-md-4">
-      <h2 class="name1">
-         Get help for anything. Done by your local Gigsters.
-      </h2>
-    </div>
-    <div class="col-md-4">
-      <h2 class="name1 ">
-        No need to spend hours on searching, We will find the right Gigster for you.
-      </h2>
-    </div>
-    <div class="col-md-4">
-      <h2 class="name1 ">
-       Available online and on mobile, making it easy to choose and communicate with your Gigster
-      </h2>
-    </div>
-    <div class="col-md-6">
-      <h2 class="name1 ">
-      No need to spend hours on searching, We will find the right Gigster for you.
-      </h2>
-    </div>
-    <div class="col-md-6">
-      <h2 class="name1 ">
-      Available online and on mobile, making it easy to choose and communicate with your Gigster
-      </h2>
-    </div>
-  </div>
-</div>
-</div>
 
 <?php
 include('footer.php'); 
