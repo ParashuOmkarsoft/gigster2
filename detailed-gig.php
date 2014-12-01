ff1921c96066846476b5a56f9417fee85d22aa39
saurabhundre <saurabh@fountaintechies.com>
2014-12-01 11:45:25 +0530

    award

--- detailed-gig.php
+++ detailed-gig.php
@@ -305,7 +305,7 @@ else
 }
 else{?>
       <div> <a data-toggle="modal" href="#awardmodal<?php echo $projectbids['rows'][$i]['bidId'];?>" >
-        <!-- <button type="button" class="btn btn-bid">Award</button> -->
+        <button type="button" class="btn btn-bid">Award</button>
         
 
         </a> </div>
