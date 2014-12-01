f28b83c5d825b204fc8ca6880d56f8047d5e17d0
saurabhundre <saurabh@fountaintechies.com>
2014-12-01 13:20:08 +0530

    completed

--- gigs-completed.php
+++ gigs-completed.php
@@ -68,7 +68,7 @@ include('cfg/more-functions.php');
 			  
 	   ?>
   <div class="row myrow"  style="border-top: none; border-bottom:none;">
-  <div class="col-md-12" style="border-bottom: 3px solid #fd8900; padding-bottom: 15px;">
+  <div class="col-md-12" style="border-bottom: 3px solid #fd8900; padding-bottom: 15px;padding:0px;">
     <div class="col-md-8" style="padding: 0;">
       <h2 id="giglisth2"><a href="<?php echo $serverpath;?>gigDetails/<?php echo mera_url_noslash($opengig['prjTitle']);?>/<?php echo $opengig['prjId'];?>"><?php echo $opengig['prjTitle'];?></a></h2> 
         <div class="col-md-8" style="padding: 0;">
@@ -188,8 +188,8 @@ include('cfg/more-functions.php');
 			  $awardedrating=get_user_rating($awarded);
 			  ?>
       	<div class="pull-right" style="padding-top: 20px;float: right;margin-left: 0px;">
-      	 <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $awardedprofilepic;?>&width=75&height=75&cropratio=1:1" style="border-radius:50px;margin-left: 40px;">
-      		<h4><?php echo $awardednametodisplay; ?></h4>
+      	 <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $awardedprofilepic;?>&width=75&height=75&cropratio=1:1" style="border-radius:50px;float:right;">
+      		<h4 style="float:right;"><?php echo $awardednametodisplay; ?></h4>
       		<div style="margin-left: 43px;">			
       					<?php 
 						 for($t=0;$t<$awardedrating;$t++)
