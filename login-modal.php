5dc912a9ee8f5910c2b8ddccf03c87d08e21f9ca
lomteankush2 <lomteankush2@gmail.com>
2014-12-02 12:14:41 -0800

    J155: Login design changes
    J155: Login design changes

--- login-modal.php
+++ login-modal.php
@@ -2,12 +2,12 @@
       <div class="modal-dialog modal-lg">
         <div class="modal-content cform">          
     <div class="container">
-       <div class="col-md-12">
+       <div class="row">
 
          <h2 id="login1">Login </h2>    
             <h2 class="source">Create a FREE account using any of your social profiles or email.</h2>  
            
-        <div class="col-md-4 fbimg"> 
+        <div class="col-md-4" style="margin-top: 55px;"> 
               <div class="modal-social-icons">
                     <a href="javascript:voide('0');" onClick="FBLogin();"><img src="<?php echo $serverpath;?>images/facebook1.png" style="padding-bottom:10px;"> </a>
                     <a target="_parent" href="<?php echo $serverpath;?>loginwithtwitter" ><img src="<?php echo $serverpath;?>images/twitter1.png" style="padding-bottom:10px;"> </a>
@@ -15,33 +15,30 @@
                     <a href='#'><img src="<?php echo $serverpath;?>images/google.png" style="padding-bottom:10px;"> </a>
               </div>
         </div>
-       <div class="col-md-8"><form class="form-horizontal hform" >
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label newlog">Username</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control passinpute" id="inputEmail3" placeholder="">
+        <div class="col-md-8" style="border-left: 1px solid #febe07;">
+          <form class="form-horizontal hform" >
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label newlog">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control passinpute" id="inputEmail3" placeholder="">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label newlog">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control passinpute" id="inputPassword3" placeholder="">
+                </div>
+              </div>    
+            <div class="form-group">
+              <div class="col-sm-offset-2 col-sm-10 logsign">
+                <button type="submit" class="btn btn-warning loginbtn">Login</button>
+                <button type="submit" class="btn btn-warning loginbtn">Signup</button>
+             
+              </div>
+            </div>
+        </form>
     </div>
   </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label newlog">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control passinpute" id="inputPassword3" placeholder="">
-    </div>
-  </div>
-  
-  
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10 logsign">
-      <button type="submit" class="btn btn-warning loginbtn">Login</button>
-      <button type="submit" class="btn btn-warning loginbtn">Signup</button>
-   
-    </div>
-  </div>
-</form>
-</div>
-
-         
-       </div>
 </div>
 </div>
 </div>
