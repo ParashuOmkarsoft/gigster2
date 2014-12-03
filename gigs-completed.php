54708450297881e1170f4274ecae7df354930670
lomteankush2 <lomteankush2@gmail.com>
2014-12-02 17:59:05 -0800

    img
    img

--- gigs-completed.php
+++ gigs-completed.php
@@ -319,7 +319,7 @@ include('cfg/more-functions.php');
 				$profilepic="images/admin.png";
 			}
 			  ?>
-			  <?php echo "imggggg    ".$profilepic;	?>
+			 
         <div class="col-md-12"> <a href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=45&height=45&cropratio=1:1"></a>
           
           
