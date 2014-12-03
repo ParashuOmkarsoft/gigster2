a0f3ee7936cf420715a1289952d0e8881132716d
lomteankush2 <lomteankush2@gmail.com>
2014-12-02 17:23:30 -0800

    img
    img

--- gigs-completed.php
+++ gigs-completed.php
@@ -308,6 +308,7 @@ include('cfg/more-functions.php');
 		    $gigsterInfo=get_user_Info(encrypt_str($profilepicId));
 			
 			$profilepic="uploads/profileimage/".$gigsterInfo['profileimage'];
+			echo "here    ".$profilepic;
 			if(file_exists($profilepic))
 			{
 				$profilepic=$profilepic;
