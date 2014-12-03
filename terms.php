03347a3794f7856e5020c0333f929f1375092a80
lomteankush2 <lomteankush2@gmail.com>
2014-12-03 12:40:15 -0800

    space
    space

--- terms.php
+++ terms.php
@@ -163,7 +163,7 @@ The GIG OWNER also expressly commits to the following:<br>
 <div class="col-md-12">
 <span id="p-name">GIG OWNER OBLIGATIONS
 </span><br>
-<p class="james">ACCESS TO THE SERVICE ON THE PLATFORM BY THE GIG OWNER<br>
+ACCESS TO THE SERVICE ON THE PLATFORM BY THE GIG OWNER<br>
 Access to the service by the GIG OWNER is subject to the acceptance of these T&Cs.<br>
 Free Services:<br>
 The GIG OWNER has access, free-of-charge, to:<br>
@@ -173,7 +173,6 @@ The GIG OWNER has access, free-of-charge, to:<br>
 
 
 Failure by the GIG OWNER to fulfill any of their obligations under this section gives GIGSTERGO.COM the right to suspend or limit its services, and to unilaterally terminate by right any interaction with the GIG OWNER, without prejudicing any legal action for interests or damages.<br>
-<div class="clearfirst"></div>
 </div>
 <strong> ACCESS TO SERVICES ON THE PLATFORM FOR THE GIGSTER</strong><br>
 The GIGSTER will provide information on the profile page, which will be accessible to (the) USER(S) in compliance with the following conditions:<br>
