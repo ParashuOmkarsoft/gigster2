fc08b00b4beec6525930a201b42d0023e41ef2d2
lomteankush2 <lomteankush2@gmail.com>
2014-12-02 21:20:21 -0800

    Image test
    Image test

--- gigs-completed.php
+++ gigs-completed.php
@@ -320,7 +320,7 @@ include('cfg/more-functions.php');
 			}
 			  ?>
 			 
-        <div class="col-md-12"> <a href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=45&height=45&cropratio=1:1"></a>
+        <div class="col-md-12"> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $profilepic;?>&width=45&height=45&cropratio=1:1"><a href="<?php echo $serverpath;?>gigsterInfo/<?php echo mera_url_noslash($nametodisplay);?>/<?php echo $gigsterInfo['userId'];?>"> </a>
           
           
           <h4>&nbsp;</h4>
