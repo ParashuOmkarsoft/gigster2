f0902187eef97efda50549dc61d6d62d94593fd9
saurabhundre <saurabh@fountaintechies.com>
2014-12-04 11:02:23 +0530

    strong

--- terms-sending.php
+++ terms-sending.php
@@ -49,7 +49,7 @@ else
 		$usernametodisplay=$userInfo['username'];
 	}
 		$mailmatter="<p>Congratulations!</p>
-				<p>Yes You have won the Gig <a href='".get_project_link($serverpath,$projectId)."'>$gigname</a>.</p>
+				<p>You have won the Gig <a href='".get_project_link($serverpath,$projectId)."'>$gigname</a>.</p>
 				<p>To update the status and see full details, please <a href='".$serverpath."acceptGig/".encrypt_str($insertQuery)."/".encrypt_str($awardedto)."'><strong>click here</strong>.</a></p>
 				<p>&nbsp;</p>
 				<p>Regards</p>
@@ -67,7 +67,7 @@ else
 							
 	}
 	$mailmatter="<p>Congratulations!</p>
-				<p>You have won the Gig <a href='".get_project_link($serverpath,$projectId)."'><strong>$gigname</strong></a>.</p>
+				<p>You have won the Gig <a href='".get_project_link($serverpath,$projectId)."'>$gigname</a>.</p>
 				<p>To update the status and see full details, please  <a href='".$serverpath."acceptGig/".encrypt_str($insertQuery)."/".encrypt_str($awardedto)."'><strong>click here</strong></a>.</p>";
 								$mailmatter=htmlentities($mailmatter);
 								$mailmatter=addslashes($mailmatter);
