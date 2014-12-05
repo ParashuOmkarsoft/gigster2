ba5ce286aba03d9cb4091a56902df7822747625a
saurabhundre <saurabh@fountaintechies.com>
2014-12-05 15:11:53 +0530

    blockqaute

--- privacy.php
+++ privacy.php
@@ -54,6 +54,7 @@ information about your usage of and interaction with our website and/or services
 <div class="row privacy-row">
 <span id="p-name">2.  Collection of your Personal Data</span><br>
 <p class="new-paragraph">1. Generally, GIGSTERGO.COM may collect your Personal Data through the following ways:<br><br>
+<blockquote >
 a.  when you sign up with us for GIGSTERGO.COM membership;<br>
 b.  when you access our websites or perform an online transaction;<br>
 c.  when you interact with any of our employees;<br>
@@ -64,7 +65,7 @@ g.  when you request that we contact you;<br>
 h.  when you respond to our initiatives or promotions; and<br>
 i.  when you submit your Personal Data to us for any other reason.<br><br>
 2.  When you browse our website, you generally do so anonymously, but please see “COOKIES”.<br>
-
+</blockquote>
 </p> 
 </div>
 
@@ -73,6 +74,7 @@ i.  when you submit your Personal Data to us for any other reason.<br><br>
 
 <span id="p-name">3.  Purposes for the Collection, Use and Disclosure of Your Personal Data</span><br><br>  
 <p class="new-paragraph">1. Generally, GIGSTERGO.COM may collect, use, disclose and/or retain your Personal Data for the following purposes:<br><br>
+<blockquote >
 a.  to manage your membership including recruitment, processing and termination of your membership;<br>
 b.  to provide you with membership benefits and services;<br>
 c.  to provide you with employment and employability services;<br>
@@ -82,6 +84,7 @@ f.  to improve membership/ customer services, such as resolving complaints and h
 g.  to conduct research, surveys and interviews;<br>
 h.  to keep you updated on our events; and<br>
 i.  to comply with applicable laws and regulations.<br>
+</blockquote>
 </p> 
 </div>
 
@@ -101,14 +104,14 @@ i.  to comply with applicable laws and regulations.<br>
 <div class="row privacy-row" style="border-bottom:none;">
 <span id="p-name">5.  Disclosure of your Personal Data </span><br><br>  
 <p class="new-paragraph" style="padding-bottom:50px;">1. GIGSTERGO.COM may disclose your Personal Data to the following group of people for purposes mentioned above, subjected to the requirements of applicable laws:<br><br>
-a.  agents, contractors, data intermediaries, employers or third party service providers who provide services, such as telecommunications, mailing, information technology, payment, payroll, data processing, training, market research, carding, storage and archival, to the Organisation;<br>
+<blockquote >a.  agents, contractors, data intermediaries, employers or third party service providers who provide services, such as telecommunications, mailing, information technology, payment, payroll, data processing, training, market research, carding, storage and archival, to the Organisation;<br>
 b.  external banks, financial institutions, credit card companies and their respective service providers;<br>
 c.  our professional advisers such as our auditors;<br>
 d.  relevant government regulators, statutory boards or authorities or law enforcement agencies to comply with any laws, rules, guidelines and regulations or schemes imposed by any government authority;<br>
 e.  third party reward, loyalty, privileges and co-branded programme providers;<br>
 f.  business partners that provides any membership services and benefits; and<br>
 g.  any other person in connection with the purposes set forth above.<br><br>
-
+</blockquote>
 <strong>1.  Protection of your Personal Data</strong><br><br>
 
 1.  GIGSTERGO.COM maintain appropriate security safeguards and practices to protect your Personal Data unauthorised access, collection, use, disclosure, copying, modification disposal or similar risks, in accordance with applicable laws.<br><br>
