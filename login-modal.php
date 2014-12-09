56288d9e7c9228db5d2cad2f472bcbac57b046b2
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-12-09 12:01:13 +0530

    sign up button

--- login-modal.php
+++ login-modal.php
@@ -31,9 +31,9 @@
               </div>    
             <div class="form-group">
               <div class="col-sm-offset-2 col-sm-10 logsign">
-                <button type="submit" value="login" name="reqType" class="btn btn-warning loginbtn-on-model">Login</button>
+                <button type="submit" value="login" name="reqType" class="btn btn-warning loginbtn-on-model" id="btnLogin">Login</button>
                 <!-- <button type="submit" class="btn btn-warning loginbtn">Signup</button> -->
-               <button type="submit" value="signup" name="reqType" class="btn btn-warning loginbtn-on-model"> Free Signup</button>
+               <button type="submit" value="signup" name="reqType" class="btn btn-warning loginbtn-on-model" onclick="javascript:document.getElementById('btnLogin').style.visibility='hidden';" style=""> Free Signup</button>
               </div>
             </div>
         </form>
