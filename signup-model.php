51124632624cffd73c5af56b93ee287a75bbefa3
saurabhundre <saurabh@fountaintechies.com>
2014-12-09 15:03:52 +0530

    Revert "learn-more"
    This reverts commit dfcc6e068c953d9d1d16cad092d04b5c92346e33.

--- signup-model.php
+++ signup-model.php
@@ -1,19 +1,48 @@
 <div id="signupmodal" class="modal fade  bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="postgigmodel" aria-hidden="true">
-       <div class="container" style="padding:40px;">
-  <div class="row clearfix">
-    <div class="col-md-12 column">
-      <h2 class="name-what-gigster text-center">
-       What Is Gigster
-      </h3>
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content cform">          
+    <div class="container">
+       <div class="col-md-12">
+
+         <h2 id="login1">Sign Up </h2>    
+            <h2 class="source">Create an account using any of your social profiles.</h2>  
+            <h2 class="source">Sign Up for a new free account Now. </h2>
+
+       <div class="col-md-8"><form class="form-horizontal hform" >
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label newlog">Username</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control passinpute" id="inputEmail3" placeholder="">
     </div>
   </div>
-  <div class="row clearfix">
-    <div class="col-md-12 column">
-      <h2 class="name-what-gigster1 text-center">
-       It’s a service that connects the people who need help, with the people who can help.
-       <br>Gigster is all about enabling you to live, work and play better. 
-      </h2>
-    </div>    
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label newlog">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control passinpute" id="inputPassword3" placeholder="">
+    </div>
   </div>
+  
+  
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10 logsign">
+      <button type="submit" class="btn btn-warning loginbtn">Sign Up</button>
+      <a data-target="#loginmodel" data-toggle="modal" data-dismiss="modal" class="btn btn-warning loginbtn">Login</a>
+    </div>
   </div>
+</form>
+</div>
+<div class="col-md-4 fbimg"> 
+<div class="modal-social-icons">
+
+                            <a href="javascript:voide('0');" onClick="FBLogin();"><img src="images/facebook1.png" style="padding-bottom:10px;"> </a>
+                            <a target="_parent" href="<?php echo $serverpath;?>loginwithtwitter" ><img src="images/twitter1.png" style="padding-bottom:10px;"> </a>
+                            <a href='<?php echo $serverpath;?>loginwithtlinkedin'><img src="images/link.png" style="padding-bottom:10px;"> </a>
+                            <a href='#'><img src="images/google.png" style="padding-bottom:10px;"> </a>
+</div>
+                        </div>
+         
+       </div>
+</div>
+</div>
+</div>
 </div>
