94527cdff9cd34a65bfb8f886e37a7e56f42a623
saurabhundre <saurabh@fountaintechies.com>
2014-12-11 13:42:24 +0530

    gig

--- my-gigs.php
+++ my-gigs.php
@@ -217,21 +217,21 @@ include('cfg/more-functions.php');
 			}
 			if($t%2==0)
 			{
-				$cl="style='text-align:left;background-color:#fdebbb;margin-top:10px;vertical-align:top;border-radius: 8px;width: 470px;'";
+				$cl="style='text-align:left;background-color:#fdebbb;margin-top:10px;vertical-align:top;border-radius: 8px;width: 470px;padding: 6px;'";
 			}
 			else
 			{
-				$cl="style='text-align:left;background-color:#fff;margin-top:10px;vertical-align:top;border-radius: 8px;width: 470px;'";
+				$cl="style='text-align:left;background-color:#fff;margin-top:10px;vertical-align:top;border-radius: 8px;width: 470px;padding: 6px;'";
 			}
 			//$updatemessage=@db_query("update btr_messages set isread='1' where msgId=".$messages['rows'][$t]['msgId']);	
 			?>
-                <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="online"/> <br/>
-                  <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small><br/>
-                    <?php echo get_user_name($msgfrom);?> </a><br/>
-                    <?php echo stripslashes(stripslashes(html_entity_decode($messages['rows'][$t]['msgcontent']))); ?><br/>
+                <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="online"/> 
+                  <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small>
+                    <?php echo get_user_name($msgfrom);?> </a>
+                    <?php echo stripslashes(stripslashes(html_entity_decode($messages['rows'][$t]['msgcontent']))); ?>
                   </p>
                 </div>
-                <br/>
+                
                 <?php
 		}
 		  ?>
