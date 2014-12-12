5ec3f54d57f1febd04b741bb20b65248bcf395ce
saurabhundre <saurabh@fountaintechies.com>
2014-12-12 09:46:37 +0530

    feedback

--- user-completed.php
+++ user-completed.php
@@ -97,20 +97,35 @@ if($checkSql['count']>0)
                   <input type="hidden" id="projectId" name="projectId" value="<?php echo $prjDetails['prjId'];?>" />
 
                   <h2 id="login1">Rate and Comment</h2>
-                  <h2 class="source"style="font-size:28px;"><?php echo $opengig['prjTitle'];?></h2>
-                  <div class="col-md-12" style="padding: 0px;">
+                  <h2 class="source"><?php echo $opengig['prjTitle'];?></h2>
+
                     <div class="form-group">
-                      <label class="col-md-2 control-label tfont" style="margin-top:14px;">Rating</label>
+                      <label class="col-md-2 control-label tfont" style="margin-bottom: 20px;">Rating</label>
                       
                       <div class="col-md-10" style="padding: 0px; margin-top:-7px">
                       
-                       
+                        <div class="form-control form-radio" >
  						<input id="rating<?php echo $prjDetails['prjId'];?>" name="rating" value="<?php echo $rt;?>" type="number" class="rating" min=0 max=5 step=0.5 data-size="sm">
 						
                          <script type="text/javascript">
 						   $('.rating').rating({'showCaption':true, 'stars':'5', 'min':'0', 'max':'5', 'step':'1', 'size':'xs'});
 						 </script>                        </div>
-                                         </div>
+                      </div>
+                    </div>
+                  <div class="col-md-12" style="padding: 0px;">
+                    <div class="form-group">
+                      <label class="col-md-2 control-label tfont" style="margin-bottom: 20px;">Rating</label>
+                      
+                      <div class="col-md-10" style="padding: 0px; margin-top:-7px">
+                      
+                        <div class="form-control form-radio" >
+ 						<input id="rating<?php echo $prjDetails['prjId'];?>" name="rating" value="<?php echo $rt;?>" type="number" class="rating" min=0 max=5 step=0.5 data-size="sm">
+						
+                         <script type="text/javascript">
+						   $('.rating').rating({'showCaption':true, 'stars':'5', 'min':'0', 'max':'5', 'step':'1', 'size':'xs'});
+						 </script>                        </div>
+                      </div>
+                    </div>
                      
                       <div class="col-sm-12">
                         <textarea class="form-control tinpute mtextarea" placeholder="Your Message" row="10" column="10" required name="experience" id="experience"></textarea>
