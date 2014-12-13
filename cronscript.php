732d4db67179d1074344df3271f27f3c7f8dca12
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-12-13 11:05:39 +0530

    cronscript

--- cronscript.php
+++ cronscript.php
@@ -3,9 +3,9 @@
 include('/home/gigster2/www/cfg/cfg.php'); 
 include('/home/gigster2/www/cfg/functions.php');
 include('/home/gigster2/www/cfg/more-functions.php'); 
-$postedfrom=mktime("0","0","0",date('m'),date('d'),date('Y'));
-$postedto=mktime("23","59","59",date('m'),date('d'),date('Y'));
 
+$postedto=time();
+$postedfrom=$postedto-(10*60*60);
 $query="select * from btr_projects where bidto>=now() and postedon>$postedfrom and postedon<$postedto order by prjId DESC ";
 $sql=@db_query($query);
 for($i=0;$i<$sql['count'];$i++)
