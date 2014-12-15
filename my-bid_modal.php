dd2428821cc300decd1db26be03aa93c60678209
saurabhundre <saurabh@fountaintechies.com>
2014-12-15 13:04:16 +0530

    br

--- my-bid_modal.php
+++ my-bid_modal.php
@@ -21,13 +21,12 @@ $opengig=get_gig_details($projectId);
       <div class="col-md-12">
         <div class="form-group">
         <label class="tooltips" style="margin-left: 15px;">Bid Details
-        <span>Other than price,sending more information usually helps.For Example, How long Gig will take oe when you are available etc.</span></label> 
+        <span>Other than price,sending more information usually helps.For Example, How long gig will take oe when you are available etc.</span></label> 
           <!-- <label for="inputText" class="col-sm-4 control-label newlog tooltips">Bid Details</label> -->
           <br/>
           <br/>
           <div class="col-sm-12">
-            <textarea class="form-control tooltips" placeholder="Bid Details" row="10" column="10" required name="proposal" id="proposal" style="width:400px;height:150px;" title=""></textarea>
-
+            <textarea class="form-control tinpute mtextarea" placeholder="Bid Details" row="10" column="10" required name="proposal" id="proposal" style="width:400px;height:150px;" title=""></textarea>
           </div>
         </div>
         <div class="form-group">
