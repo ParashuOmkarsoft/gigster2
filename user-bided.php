13dce644f8eaed92a52e57783f56ee21eaa81049
saurabhundre <saurabh@fountaintechies.com>
2014-12-15 16:30:24 +0530

    user-bidded

--- user-bided.php
+++ user-bided.php
@@ -137,14 +137,14 @@ if(!is_project_awarded_to_user($prjDetails['prjId'],$uInfo['userId']))
 			}
 			//$updatemessage=@db_query("update btr_messages set isread='1' where msgId=".$messages['rows'][$t]['msgId']);	
 			?>
-                  <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="img-circle"/> <br/>
-                    <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small><br/>
+                  <div class="item" <?php echo $cl;?>> <img src="<?php echo $serverpath;?>image.php?image=/<?php echo $buserimage;?>&width=50&height=50&cropratio=1:1" alt="<?php echo get_user_name($msgfrom);?>" class="img-circle" style="float:none;margin-top:0px;"/> <br/>
+                    <p class="message"> <a href="#" class="name"><small class="text-muted pull-right"><i class="fa fa-clock-o"></i>&nbsp; <?php echo gmstrftime("%B %d %Y, %X %p",$messages['rows'][$t]['msgon']);?></small>
                       <?php echo get_user_name($msgfrom);?> </a><br/>
                       <?php echo stripslashes(stripslashes(html_entity_decode($messages['rows'][$t]['msgcontent'])));
 					  ?><br/>
                     </p>
                   </div>
-                  <br/>
+                 
                   <?php
 		}
 		  ?>
