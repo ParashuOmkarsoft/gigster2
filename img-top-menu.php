e8156cb2a0b30a2ec006af9c47abfeb7f0654499
saurabhundre <saurabh@fountaintechies.com>
2014-12-16 11:39:18 +0530

    index

--- img-top-menu.php
+++ img-top-menu.php
@@ -5,6 +5,18 @@
             <a href="<?php echo $serverpath;?>"><img src="<?php echo $serverpath;?>images/logo-1.png" class="logo"></a>
           </div>
           <div class="col-md-10">
+          <nav class="navbar navbar-default hmenu" role="navigation">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+    		    </div>
+
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                 <?php if(isset($_SESSION['uId']))
