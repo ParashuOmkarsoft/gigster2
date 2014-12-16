b574cf819dad836d7c4b66103bc54c905966de73
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-12-16 16:14:45 +0530

    j187

--- report-sending.php
+++ report-sending.php
@@ -16,9 +16,9 @@ if($insertSql)
 	$projectbidder=get_user_Info(encrypt_str($reportfrom));
 	$projectdetails=get_gig_details($projectId);
 	$messagecontent="<p>Hello ".$projectowner['username']."</p>";
-	$messagecontent.="<p>You have received a new status report on your Gig <strong>".$projectdetails['prjTitle']."</strong> from <strong>".$projectbidder['username']."</strong>.</p>";
-	$messagecontent.="<p><strong>Message</strong><br/>$message</p>";
-	$messagecontent.="<p><strong>Completion Status </strong><br/>$completed %</p>";	
+	$messagecontent.="<p>You have received a new status report on your Gig <font style='color:#832b00;'>".$projectdetails['prjTitle']."</font> from <font style='color:#832b00;'>".$projectbidder['username']."</font>.</p>";
+	$messagecontent.="<p><font style='color:#832b00;'>Message</font><br/>$message</p>";
+	$messagecontent.="<p><font style='color:#832b00;'>Completion Status </font><br/>$completed %</p>";	
 	$messagecontent=htmlentities($messagecontent);
 	$messagequery="insert into btr_messages(msgfrom,msgto,msgcontent,haveattachment,msgon,projectId,isread,msgtype,reportid)";
 	$messagequery.="values($reportfrom,$reportto,'$messagecontent','0',".time().",$projectId,'0','s',$insertSql)";	
