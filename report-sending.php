a253ceca40ba8ad71d1e6afab432f3ed334d1e1a
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-12-16 18:35:00 +0530

    j187

--- report-sending.php
+++ report-sending.php
@@ -16,10 +16,12 @@ if($insertSql)
 	$projectbidder=get_user_Info(encrypt_str($reportfrom));
 	$projectdetails=get_gig_details($projectId);
 	$messagecontent="<p>Hello ".$projectowner['username']."</p>";
-	$messagecontent.="<p>You have received a new status report on your Gig <font style='color:#832b00;'>".$projectdetails['prjTitle']."</font> from <font style='color:#832b00;'>".$projectbidder['username']."</font>.</p>";
+	$messagecontent.="<p>You have received a new status report on your Gig <a href='".get_project_link($serverpath,$projectId)."'><font style='color:#832b00;'>".$projectdetails['prjTitle']."</font></a> from <font style='color:#832b00;'>".$projectbidder['username']."</font>.</p>";
 	$messagecontent.="<p><font style='color:#832b00;'>Message</font><br/>$message</p>";
 	$messagecontent.="<p><font style='color:#832b00;'>Completion Status </font><br/>$completed %</p>";	
 	$messagecontent=htmlentities($messagecontent);
+	$messagecontent=addslashes($messagecontent);
+//	$message
 	$messagequery="insert into btr_messages(msgfrom,msgto,msgcontent,haveattachment,msgon,projectId,isread,msgtype,reportid)";
 	$messagequery.="values($reportfrom,$reportto,'$messagecontent','0',".time().",$projectId,'0','s',$insertSql)";	
 	$messagesql=@db_query($messagequery,3);
@@ -30,6 +32,10 @@ if($insertSql)
 		window.parent.location="<?php echo $_SERVER['HTTP_REFERER'];?>";
 		</script>
 		<?php
+	}
+	else
+	{
+		print_r($GLOBALS['debug_sql']);
 	}
 }
 else{
