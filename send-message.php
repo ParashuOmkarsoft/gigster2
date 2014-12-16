fca3683e36b2cd980ce20bdba190860531a695eb
Infodreams Solutions <infodreamssolutions@gmail.com>
2014-12-16 17:27:44 +0530

    j195

--- send-message.php
+++ send-message.php
@@ -54,7 +54,7 @@ if($messageSql)
 				<p>To respond to your messages, go straight to your <a href='".$serverpath."inbox'>Inbox</a>.</p>
 				<p>GigsterGo.com</p>
 				<p>________________________________________</p>
-<p style='font-style:italic;'>GigsterGo sends these emails based on the preferences you set for your account. To change your communication preferences and adjust your skill matches, <a href='".$serververpath."'>login</a> and go to your <a href='".$serververpath."myaccount'>Profile</a>. </p>
+<p style='font-style:italic;'>GigsterGo sends these emails based on the preferences you set for your account. To change your communication preferences and adjust your skill matches, <a href='".$serverpath."'>login</a> and go to your <a href='".$serververpath."myaccount'>Profile</a>. </p>
 				";
 						if($toInfo['notify']=="1")
 						{
